946f2790c0a59b19fee664135dcd453eff92c1e9
Jérémy Lal <kapouer@melix.org>
2017-03-13 18:33:44 +0100

    Do not depend on semantic icons for now, cleanup

--- packages/elements-write/public/pageboard/breadcrumb.js
+++ packages/elements-write/public/pageboard/breadcrumb.js
@@ -5,16 +5,23 @@ function Breadcrumb(editor, selector) {
 	this.$node = $(selector);
 	this.editor = editor;
 	this.template = this.$node[0].cloneNode(true);
-	this.$node.empty();
+	this.clear();
 	this.$node.on('click', 'a', this.click);
 }
 
 Breadcrumb.prototype.update = function(parents) {
-	this.$node.empty();
+	this.clear();
 	for (var i=0; i < parents.length; i++) {
 		var block = parents[i].block;
 		this.$node.append(this.item(block));
 	}
+};
+
+Breadcrumb.prototype.clear = function() {
+	this.$node.empty();
+	var node = this.template.cloneNode(true);
+	$(node).find('a').remove();
+	this.$node.append(node);
 };
 
 Breadcrumb.prototype.item = function(block) {
