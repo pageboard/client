0705bf0a0cfa3c39c7541c2f2fdc17d7d691845a
Jérémy Lal <kapouer@melix.org>
2017-03-14 09:56:30 +0100

    Build menus and dropdowns

--- packages/elements-write/public/pageboard/menu.js
+++ packages/elements-write/public/pageboard/menu.js
@@ -9,62 +9,86 @@ Pageboard.setupMenu = function(selector, editor) {
 	return menu;
 };
 
-function getMenuItems(main) {
-	var items = [];
-	var schema = main.view.state.schema;
-	var win = main.view.root.defaultView;
-
-	// TODO
-
-	// this is how the menu looks
-
-	/*
-	[[inlineItem1, inlineItem2, ...], [dropDownWithIcon1, dropDownWithIcon2, ...]]
-	inline items are always shown (but there are very few of them)
-	dropDowns are always shown, and are "opened" automatically when focusing a block
-	the dropDowns icons are not configurable
-	the menu items in dropDowns can configure their display as usual, but we'll put their name for now
-	*/
-
-	for (var i=0; i < main.elements.length; i++) {
-		var el = main.elements[i];
-		var nodeType = schema.nodes[el.name] || schema.marks[el.name];
-		if (!nodeType) continue;
-		if (!el.icon) continue;
-
-		items.push(new Pagecut.Menubar.Menu.MenuItem({
-			title: el.name,
-			onDeselected: 'disable',
-			icon: el.icon,
-			run: function(state, dispatch, view) {
-				var block = {
-					id: - Date.now(),
-					type: this.name
-				};
-				main.modules.id.set(block);
-
-				if (el.inline) {
-					// TODO select whole node to do that
-					win.Pagecut.Commands.toggleMark(
-						nodeType,
-						{block_id: block.id}
-					)(state, dispatch);
-				} else {
-					var node = main.parse(main.render(block, true)).content[0];
-					dispatch(state.tr.replaceSelectionWith(node));
-				}
-			}.bind({name: el.name}),
-			select: function(state) {
-				if (el.inline) return true;
+function getItem(editor, el, nodeType) {
+	return {
+		title: el.name,
+		onDeselected: 'disable',
+		icon: el.icon,
+		run: function(state, dispatch, view) {
+			// TODO give a way to remove marks of current selected root node(s)
+			var block = {
+				id: - Date.now(),
+				type: el.name
+			};
+			editor.modules.id.set(block);
+			editor.replace(state.tr.selection, block);
+		},
+		select: function(state) {
+			if (el.inline) {
+				return canMark(state, nodeType);
+			} else {
 				return canInsert(state, nodeType);
-			},
-			active: function(state) {
-				if (!el.inline) return true;
+			}
+		},
+		active: function(state) {
+			if (!el.inline) {
+				var parents = editor.selectionParents(state.tr.selection);
+				var parent = parents[parents.length - 1];
+				return parent && parent.block.type == el.name;
+			} else {
 				return markActive(state, nodeType);
 			}
+		}
+	};
+}
+
+function getMenuItems(editor) {
+	var singles = [];
+	var dropdowns = [];
+	var items = [singles, dropdowns];
+	var menuGroups = {};
+	var groups = [];
+	var schema = editor.view.state.schema;
+
+	var group, item;
+	for (var i=0; i < editor.elements.length; i++) {
+		var el = editor.elements[i];
+		var nodeType = schema.nodes[el.name] || schema.marks[el.name];
+		if (!nodeType || (!el.menu && !el.icon)) continue;
+		item = new Pagecut.Menubar.Menu.MenuItem(getItem(editor, el, nodeType));
+		if (el.menu) {
+			group = menuGroups[el.menu]
+			if (!group) {
+				group = menuGroups[el.menu] = [];
+				group.menu = el.menu;
+				groups.push(group);
+			}
+			group.push(item);
+		} else if (el.icon) {
+			singles.push(item);
+		}
+	}
+
+	for (var i=0; i < groups.length; i++) {
+		dropdowns.push(new Pagecut.Menubar.Menu.Dropdown(groups[i], {
+			label: groups[i].menu,
+			title: "my title",
+			class: "myclass",
+			css: "mycss"
 		}));
 	}
-	return [items];
+
+	return items;
+}
+
+function canMark(state, nodeType) {
+	var can = state.doc.contentMatchAt(0).allowsMark(nodeType);
+	var sel = state.tr.selection;
+	state.doc.nodesBetween(sel.from, sel.to, function(node) {
+		if (can) return false;
+		can = node.isTextblock && node.contentMatchAt(0).allowsMark(nodeType);
+	});
+	return can;
 }
 
 function canInsert(state, nodeType, attrs) {
