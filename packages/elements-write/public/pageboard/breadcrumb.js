a10450a90b813ba3091c349e310c88b8ecb13adb
Jérémy Lal <kapouer@melix.org>
2017-03-23 01:21:47 +0100

    Way nicer breadcrumb

--- packages/elements-write/public/pageboard/breadcrumb.js
+++ packages/elements-write/public/pageboard/breadcrumb.js
@@ -6,37 +6,42 @@ function Breadcrumb(editor, selector) {
 	this.editor = editor;
 	this.template = this.$node[0].cloneNode(true);
 	this.clear();
-	this.$node.on('click', 'a', this.click.bind(this));
+	this.$node.on('click', '.section', this.click.bind(this));
 }
 
 Breadcrumb.prototype.update = function(parents) {
 	this.clear();
+	var parent;
 	for (var i = parents.length - 1; i >= 0; i--) {
-		var block = parents[i].block;
-		this.$node.append(this.item(block));
+		parent = parents[i];
+		this.$node.append(this.item(parent.block));
 	}
+	if (parent && parent.content && parent.content.name) {
+		var contentTitle = this.editor.map[parent.block.type].contents[parent.content.name].title;
+		if (contentTitle) {
+			this.$node.append($('<span class="section">' + contentTitle + '</span>'));
+		}
+	}
+	this.$node.find('.section').last().addClass('active').next('.divider').remove();
 };
 
 Breadcrumb.prototype.clear = function() {
 	this.$node.empty();
-	var node = this.template.cloneNode(true);
-	$(node).find('a').remove();
-	this.$node.append(node);
 };
 
 Breadcrumb.prototype.item = function(block) {
 	var node = this.template.cloneNode(true);
-	var anchor = node.querySelector('a');
-	anchor.textContent = this.editor.map[block.type].title;
-	anchor.setAttribute('block-id', block.id);
-	return node;
+	var item = node.querySelector('.section');
+	item.textContent = this.editor.map[block.type].title;
+	item.setAttribute('block-id', block.id);
+	return node.innerHTML;
 }
 
 Breadcrumb.prototype.click = function(e) {
 	var id = e.target.getAttribute('block-id');
 	var node = this.editor.view.root.querySelector('[block-id="'+id+'"]');
 	if (!node) console.warn("block node not found", id);
-	var sel = this.editor.select(node, true);
+	var sel = this.editor.select(node);
 	if (sel) {
 		this.editor.view.dispatch(this.editor.view.state.tr.setSelection(sel));
 		this.editor.view.focus();
