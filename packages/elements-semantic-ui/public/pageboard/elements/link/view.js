5ba93492acdbd77088ad56945f9366dd4509c58c
Jérémy Lal <kapouer@melix.org>
2017-03-29 12:20:57 +0200

    Clean view link function

--- packages/elements-semantic-ui/public/pageboard/elements/link/view.js
+++ packages/elements-semantic-ui/public/pageboard/elements/link/view.js
@@ -4,9 +4,8 @@ if (!exports.link) exports.link = {};
 
 exports.link.view = function(doc, block) {
 	var anchor = doc.createElement('a');
-	anchor.setAttribute('href', block.data.url || 'http://google.com');
+	anchor.setAttribute('href', block.data.url || '');
 	if (block.data.target) anchor.setAttribute('target', block.data.target);
-	// anchor.setAttribute("block-content", "content");
 	return anchor;
 };
 
