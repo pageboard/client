e589a50b46a348587e7e4f5d4bf8edf056a993d5
Jérémy Lal <kapouer@melix.org>
2017-04-18 16:18:17 +0200

    Improve notifications

--- packages/elements-write/public/pageboard/notification.js
+++ packages/elements-write/public/pageboard/notification.js
@@ -1,27 +1,52 @@
 (function(Pageboard) {
 
 var root;
+var list = [];
 
 Pageboard.notify = function(title, text) {
-	var parent = Pageboard.notify.dom();
-	parent.insertAdjacentHTML('beforeEnd', [
-	'<div class="ui attached negative message">',
-		'<i class="close icon"></i>',
-		'<div class="header">',
-			title,
-		'</div>',
-		text ? '<p>' + text + '</p>' : '',
-	'</div>'
-	].join('\n'));
-	var msg = parent.lastChild;
-	msg.addEventListener('click', function() {
-		msg.remove();
+	text = text.toString();
+	if (list.length) {
+		var last = list[list.length - 1];
+		if (last.title == title && last.text == text) {
+			// do nothing
+			console.info("Repeated error", title);
+			return;
+		}
+	}
+	list.push({
+		title: title,
+		text: text
 	});
+	var parent = Pageboard.notify.dom();
+
+	var msg = html`<div class="ui attached negative message">
+		<i class="close icon"></i>
+		<div class="header">${title}</div>
+		${withText(text)}
+	</div>`;
+
+	parent.appendChild(msg);
 };
+
+function withText(text) {
+	if (text) return html`<p>${text}</p>`;
+	else return '';
+}
 
 Pageboard.notify.dom = function() {
 	if (root) return root;
 	root = document.getElementById('notifications');
+	root.addEventListener('click', function(e) {
+		var msg = e.target.closest('.message');
+		if (!msg) return;
+		var index = 0;
+		var cur = msg;
+		while (cur=cur.previousSibling) {
+			index++;
+		}
+		list.splice(index, 1);
+		msg.remove();
+	});
 	return root;
 };
 
