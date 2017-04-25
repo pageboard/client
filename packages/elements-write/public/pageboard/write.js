37c9a2f72006ae03fe0affb7e5c77f58b8fb0c93
Jérémy Lal <kapouer@melix.org>
2017-04-25 14:29:14 +0200

    Pageboard.trigger helper

--- packages/elements-write/public/pageboard/write.js
+++ packages/elements-write/public/pageboard/write.js
@@ -1,5 +1,12 @@
 window.Pagecut = {modules:{}};
-window.Pageboard = {Controls: {}};
+window.Pageboard = {
+	Controls: {},
+	trigger: function(node, event) {
+		var e = document.createEvent('Event');
+		e.initEvent(event, true, true);
+		node.dispatchEvent(e);
+	}
+};
 
 Page.setup(function(state) {
 	Pageboard.setup();
