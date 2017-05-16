9825c8d3968b08d7ede0abe5fd237eef3570ffb8
Jérémy Lal <kapouer@melix.org>
2017-05-16 12:24:42 +0200

    Experimental element with evaluated foreign content

---
+++ packages/elements-semantic-ui/public/pageboard/elements/literal.js
@@ -0,0 +1,31 @@
+(function(exports) {
+
+exports.literal = {
+	title: "Literal",
+	properties: {
+		code: {
+			title: 'Javascript accessor',
+			description: 'A global variable to access',
+			type: "string",
+			pattern: "((^|\.)[a-zA-Z0-9_]+)+$"
+		}
+	},
+	inline: true,
+	icon: '<b class="icon">${...}</b>',
+	view: function(doc, block) {
+		return doc.dom`<code class="literal">${block.data.code || 'uninitialized literal'}</code>`;
+	},
+	foreign: function(dom, block) {
+		if (block.data.code == null) return;
+		var result = "";
+		try {
+			result = eval(block.data.code);
+		} catch(ex) {
+			return;
+		}
+		dom.textContent = result;
+	}
+};
+
+})(typeof exports == "undefined" ? window.Pagecut.modules : exports);
+
