6904cf75cc4df27c17160334b4f28ea07dd65165
Jérémy Lal <kapouer@melix.org>
2017-08-08 01:11:19 +0200

    Reselect after insertion

--- packages/elements-write/write/menu.js
+++ packages/elements-write/write/menu.js
@@ -40,13 +40,25 @@ Menu.prototype.item = function(el) {
 			editor.blocks.set(block);
 			var tr = state.tr;
 			var sel = editor.utils.selectTr(tr, tr.selection, true);
+			var inserts = false;
 
 			if (el.inline && editor.utils.markActive(state, nodeType)) {
 				tr = tr.removeMark(sel.from, sel.to, nodeType);
 			} else {
 				tr = editor.utils.insertTr(tr, block, sel);
+				inserts = true;
 			}
-			if (tr) dispatch(tr);
+			dispatch(tr);
+			if (!inserts) return;
+			var node = editor.blocks.domQuery(block.id);
+			if (!node) {
+				throw new Error(`No node found with block-id ${block.id}`);
+			}
+			var sel = editor.utils.select(node);
+			if (sel) {
+				editor.dispatch(editor.state.tr.setSelection(sel));
+				editor.focus();
+			}
 		},
 		select: function(state) {
 			if (el.inline) {
