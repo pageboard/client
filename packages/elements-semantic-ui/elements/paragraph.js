b13b2bc13b656a96189e2417af33df58b3724ca6
Jérémy Lal <kapouer@melix.org>
2017-08-16 17:35:39 +0200

    paragraph is a native element

--- packages/elements-semantic-ui/elements/paragraph.js
+++ packages/elements-semantic-ui/elements/paragraph.js
@@ -1,18 +1,12 @@
 Pageboard.elements.paragraph = {
 	title: "Paragraph",
-	generic: true, // means it is matched against nodeName, not block-type attribute
 	priority: -10,
 	properties: {},
-	contents: {
-		text: {
-			spec: "inline<_>*",
-			title: 'Text'
-		}
-	},
+	contents: "inline<_>*",
 	group: "block",
 	icon: '<i class="icon paragraph"></i>',
 	render: function(doc, block) {
-		return doc.dom`<p block-content="text"></p>`;
+		return doc.dom`<p></p>`;
 	}
 };
 
