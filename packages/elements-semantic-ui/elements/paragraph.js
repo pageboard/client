702f07bcd73cbe4349952f06eb279e724b41be9e
Jérémy Lal <kapouer@melix.org>
2017-08-22 11:10:07 +0200

    paragraph is inplace

--- packages/elements-semantic-ui/elements/paragraph.js
+++ packages/elements-semantic-ui/elements/paragraph.js
@@ -4,6 +4,7 @@ Pageboard.elements.paragraph = {
 	properties: {},
 	contents: "inline<_>*",
 	group: "block",
+	inplace: true,
 	icon: '<i class="icon paragraph"></i>',
 	render: function(doc, block) {
 		return doc.dom`<p></p>`;
