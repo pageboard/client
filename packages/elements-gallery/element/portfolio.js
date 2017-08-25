78d2707a9eff8af6523f0f53a2ae3cbc171315ef
Jérémy Lal <kapouer@melix.org>
2017-08-25 10:46:35 +0200

    A nice and simple portfolio

--- packages/elements-gallery/element/portfolio.js
+++ packages/elements-gallery/element/portfolio.js
@@ -1,27 +1,15 @@
 Pageboard.elements.portfolio = {
 	title: "Portfolio",
-	properties: {
-		numCols: {
-			title: 'Number of columns',
-			description: 'Number of unit columns per row',
-			type: 'number',
-			minimum: 1,
-			maximum: 5,
-			default: 4
-		}
-	},
 	contents: {
 		items: {
-			spec: "portfolio_item*",
+			spec: "portfolio_item+",
 			title: 'items'
 		}
 	},
 	group: 'block',
 	icon: '<b class="icon">Por</b>',
 	render: function(doc, block, view) {
-		var num = ["", "one", "two", "three", "four", "five"][block.data.numCols];
-		if (num) num += " wide";
-		return doc.dom`<element-portfolio class="${num}" block-content="items"></element-portfolio>`;
+		return doc.dom`<element-portfolio block-content="items"></element-portfolio>`;
 	},
 	stylesheets: [
 		'../ui/portfolio.css'
@@ -37,33 +25,63 @@ Pageboard.elements.portfolio_item = {
 	title: "Item",
 	properties: {
 		ratio: {
-			title: 'Ratio',
-			description: 'Cell ratio',
+			title: 'Size',
+			description: 'Cell size',
 			oneOf: [{
-				constant: "one-one",
+				constant: "1:1",
 				title: "1:1"
 			}, {
-				constant: "one-two",
+				constant: "1:2",
 				title: "1:2"
 			}, {
-				constant: "two-one",
+				constant: "2:1",
 				title: "2:1"
 			}, {
-				constant: "two-two",
+				constant: "2:2",
 				title: "2:2"
 			}],
-			default: "one-one"
+			default: "1:1"
+		},
+		url: {
+			title: 'Image',
+			description: 'Local or remote URL',
+			type: "string",
+			format: "uri"
 		}
 	},
 	contents: {
 		cell: {
-			spec: "block+",
+			spec: "inline+",
 			title: "cell"
 		}
 	},
 	icon: '<b class="icon">Cell</b>',
 	render: function(doc, block) {
-		return doc.dom`<div block-content="cell" class="item ${block.data.ratio}"></div>`;
+		var ratio = block.data.ratio.split(':');
+		var url = block.data.url;
+		var img = "";
+		if (url) {
+			var sep = '?';
+			if (url.startsWith('/') == false) {
+				url = ".api/image?url=" + encodeURIComponent(url);
+				sep = '&';
+			}
+			var w = 100 * ratio[0];
+			var h = 100 * ratio[1];
+			img = doc.dom`<img src="${url}"
+				srcset="${url}${sep}rs=w:${w}%2Ch=${h} 160w,
+				${url}${sep}rs=w:${w}%2Ch=${h} 320w,
+				${url}${sep}rs=w:${2*w}%2Ch=${2*h} 640w,
+				${url}${sep}rs=w:${3*w}%2Ch=${3*h} 1280w" />`;
+			console.log("generates img", img);
+		} else {
+			console.log("no img", block);
+		}
+		var large = ratio[0] == 2 ? 'large' : '';
+		return doc.dom`<div class="item ${large}">
+			<div class="image">${img}</div>
+			<div block-content="cell"></div>
+		</div>`;
 	}
 };
 
