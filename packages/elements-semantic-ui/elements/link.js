254ef6a904a7c91bd509f83707b946c6763745e2
Jérémy Lal <kapouer@melix.org>
2017-08-25 22:13:25 +0200

    link: show url last

--- packages/elements-semantic-ui/elements/link.js
+++ packages/elements-semantic-ui/elements/link.js
@@ -1,12 +1,6 @@
 Pageboard.elements.link = {
 	title: "Link",
 	properties: {
-		url: {
-			title: 'Address',
-			description: 'Local or remote URL',
-			type: "string",
-			format: "uri"
-		},
 		target: {
 			title: 'Target',
 			description: 'Choose how to open link',
@@ -27,6 +21,12 @@ Pageboard.elements.link = {
 			description: 'Show link as button',
 			type: 'boolean',
 			default: false
+		},
+		url: {
+			title: 'Address',
+			description: 'Local or remote URL',
+			type: "string",
+			format: "uri"
 		}
 	},
 	contents: {
