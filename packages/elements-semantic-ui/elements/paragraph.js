b938c53d1b03d7a8219c33d7fc0b341480ae4b72
Jérémy Lal <kapouer@melix.org>
2017-09-04 18:51:50 +0200

    heading element

--- packages/elements-semantic-ui/elements/paragraph.js
+++ packages/elements-semantic-ui/elements/paragraph.js
@@ -11,3 +11,28 @@ Pageboard.elements.paragraph = {
 	}
 };
 
+Pageboard.elements.heading = {
+	title: "Heading",
+	properties: {
+		level: {
+			title: 'Level',
+			description: 'Between 1 and 6',
+			type: "integer",
+			default: 1,
+			minimum: 1,
+			maximum: 6
+		}
+	},
+	contents: "inline<_>*",
+	group: "block",
+	inplace: true,
+	icon: '<i class="icon header"></i>',
+	tag: 'h1,h2,h3,h4,h5,h6',
+	parse: function(dom) {
+		var level = parseInt(dom.nodeName.substring(1));
+		return {level: level};
+	},
+	render: function(doc, block) {
+		return doc.createElement('h' + block.data.level);
+	}
+};
