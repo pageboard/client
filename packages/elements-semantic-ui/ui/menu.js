7d22818d381b33169a67de3da949e2859723ebff
Jérémy Lal <kapouer@melix.org>
2017-09-06 02:37:53 +0200

    Compare host with menu too

--- packages/elements-semantic-ui/ui/menu.js
+++ packages/elements-semantic-ui/ui/menu.js
@@ -2,7 +2,9 @@ Page.build(function(state) {
 	Array.prototype.forEach.call(
 		document.querySelectorAll('.ui.menu [href]'),
 		function(item) {
-			if (item.pathname == state.pathname) item.classList.add('active');
+			if (item.host == state.host && item.pathname == state.pathname) {
+				item.classList.add('active');
+			}
 		}
 	);
 });
