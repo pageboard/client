614edefa84a704837e2592e9453591baab78ffff
Jérémy Lal <kapouer@melix.org>
2017-09-14 02:32:51 +0200

    Ability to change portfolio items appearance

--- packages/elements-gallery/element/portfolio.js
+++ packages/elements-gallery/element/portfolio.js
@@ -1,6 +1,18 @@
 Pageboard.elements.portfolio = {
 	title: "Portfolio",
-	properties: {},
+	properties: {
+		shape: {
+			title: 'Shape',
+			oneOf: [{
+				constant: "square",
+				title: "Square",
+			}, {
+				constant: "rectangle",
+				title: "Rectangle"
+			}],
+			default: "square"
+		}
+	},
 	contents: {
 		items: {
 			spec: "portfolio_item+",
@@ -9,8 +21,8 @@ Pageboard.elements.portfolio = {
 	},
 	group: 'block',
 	icon: '<b class="icon">Por</b>',
-	render: function(doc, block, view) {
-		return doc.dom`<element-portfolio block-content="items"></element-portfolio>`;
+	render: function(doc, block) {
+		return doc.dom`<element-portfolio block-content="items" data-shape="${block.data.shape}"></element-portfolio>`;
 	},
 	stylesheets: [
 		'../ui/portfolio.css'
@@ -24,23 +36,27 @@ Pageboard.elements.portfolio = {
 Pageboard.elements.portfolio_item = {
 	title: "Item",
 	properties: {
-		ratio: {
-			title: 'Size',
-			description: 'Cell size',
+		scaleWidth: {
+			title: 'Scale width',
 			oneOf: [{
-				constant: "1:1",
-				title: "1:1"
+				constant: "1",
+				title: "100%"
 			}, {
-				constant: "1:2",
-				title: "1:2"
-			}, {
-				constant: "2:1",
-				title: "2:1"
-			}, {
-				constant: "2:2",
-				title: "2:2"
+				constant: "2",
+				title: "200%"
 			}],
-			default: "1:1"
+			default: "1"
+		},
+		scaleHeight: {
+			title: 'Scale height',
+			oneOf: [{
+				constant: "1",
+				title: "100%"
+			}, {
+				constant: "2",
+				title: "200%"
+			}],
+			default: "1"
 		},
 		url: {
 			title: 'Image',
@@ -61,30 +77,8 @@ Pageboard.elements.portfolio_item = {
 	},
 	icon: '<b class="icon">Cell</b>',
 	render: function(doc, block) {
-		var url = block.data.url;
-		var img = "";
-		if (url) {
-			var sep = '?';
-			if (url.startsWith('/') == false) {
-				url = ".api/image?url=" + encodeURIComponent(url);
-				sep = '&';
-			}
-
-			var ratio = block.data.ratio.split(':');
-			var vals = {"1": 97, "2": 197};
-			// see portfolio.css for how to compute those numbers
-			var w = vals[ratio[0]];
-			var h = vals[ratio[1]];
-			var large = ratio[0] == 2;
-
-			img = doc.dom`<img src="${url}"
-				srcset="${url}${sep}rs=w:${w}%2Ch:${h}%2Cenlarge 160w,
-				${url}${sep}rs=w:${2*w}%2Ch:${2*h}%2Cenlarge 320w,
-				${url}${sep}rs=w:${4*w}%2Ch:${4*h}%2Cenlarge 640w,
-				${url}${sep}rs=w:${8*w}%2Ch:${8*h}%2Cenlarge 1280w" />`;
-		}
-		return doc.dom`<div class="portfolio item ${large ? 'large' : ''}">
-			<div class="portfolio image">${img}</div>
+		return doc.dom`<div class="portfolio item" data-scale-width="${block.data.scaleWidth}" data-scale-height="${block.data.scaleHeight}">
+			<element-portfolio-image src="${block.data.url}"></element-portfolio-image>
 			<div class="portfolio cell" block-content="cell"></div>
 		</div>`;
 	}
--- packages/elements-gallery/ui/portfolio.js
+++ packages/elements-gallery/ui/portfolio.js
@@ -43,10 +43,54 @@ class HTMLElementPortfolio extends HTMLElement {
 
 	update() {
 		this._setup();
+		// TODO update isn't called when changing the portfolio properties
+		Array.from(this.querySelectorAll('element-portfolio-image')).forEach(function(node) {
+			node.update();
+		});
+	}
+}
+
+class HTMLElementPortfolioImage extends HTMLElement {
+	constructor() {
+		super();
+		this.img = this.ownerDocument.createElement('img');
+		this.appendChild(this.img);
+	}
+	update() {
+		var url = this.getAttribute('src');
+		if (!url) {
+			this.img.removeAttribute('src');
+			return;
+		}
+		var portfolio = this.closest('element-portfolio');
+		if (!portfolio) return;
+		var item = this.closest('[block-type="portfolio_item"]');
+		if (!item) return;
+		var sep = '?';
+		if (url.startsWith('/') == false) {
+			url = ".api/image?url=" + encodeURIComponent(url);
+			sep = '&';
+		}
+
+		// see portfolio.css for how to compute those numbers
+		var sizes = {"1": 97, "2": 197};
+		var shape = portfolio.dataset.shape;
+		if (sizes[shape] == null) shape = "square";
+		var w = sizes[item.dataset.scaleWidth || "1"];
+		var h = sizes[item.dataset.scaleHeight || "1"] * (shape == "rectangle" ? 1.6 : 1.0);
+
+		this.img.srcset = `${url}${sep}rs=w:${w}%2Ch:${h}%2Cenlarge 160w,
+			${url}${sep}rs=w:${2*w}%2Ch:${2*h}%2Cenlarge 320w,
+			${url}${sep}rs=w:${4*w}%2Ch:${4*h}%2Cenlarge 640w,
+			${url}${sep}rs=w:${8*w}%2Ch:${8*h}%2Cenlarge 1280w`;
+	}
+	connectedCallback() {
+		this.update();
 	}
 }
 
 Page.setup(function() {
 	window.customElements.define('element-portfolio', HTMLElementPortfolio);
+	window.customElements.define('element-portfolio-image', HTMLElementPortfolioImage);
 });
 
