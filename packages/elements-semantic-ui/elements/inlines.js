d3667bb756c907d56ce0d6a06b7342ed0b1c50b0
Jérémy Lal <kapouer@melix.org>
2017-09-15 10:58:39 +0200

    Commit colors

--- packages/elements-semantic-ui/elements/inlines.js
+++ packages/elements-semantic-ui/elements/inlines.js
@@ -33,3 +33,58 @@ Pageboard.elements.light = {
 		return doc.dom`<span class="lighter"></span>`;
 	}
 };
+
+Pageboard.elements.color = {
+	title: "Color",
+	properties: {
+		color: {
+			default: "",
+			oneOf: [{
+				constant: "white",
+				title: "White"
+			}, {
+				constant: "",
+				title: "Black"
+			}, {
+				constant: "red",
+				title: "Red"
+			}, {
+				constant: "orange",
+				title: "Orange"
+			}, {
+				constant: "yellow",
+				title: "Yellow"
+			}, {
+				constant: "green",
+				title: "Green"
+			}, {
+				constant: "blue",
+				title: "Blue"
+			}, {
+				constant: "purple",
+				title: "Purple"
+			}]
+		}
+	},
+	parse: function(dom) {
+		var prop = Pageboard.elements.color.properties.color;
+		var color = prop.oneOf.find(function(item) {
+			return item.constant && dom.classList.contains(item.constant);
+		});
+		if (color) color = color.constant;
+		else color = prop.default;
+		return {color: color};
+	},
+	contents: "text*",
+	inline: true,
+	inplace: true,
+	group: "inline",
+	icon: `<i class="icon" style="background-image:
+		linear-gradient(
+			to right,
+			red, orange, yellow, green, blue, purple
+		);"></i>`,
+	render: function(doc, block) {
+		return doc.dom`<span class="${block.data.color} color"></span>`;
+	}
+};
