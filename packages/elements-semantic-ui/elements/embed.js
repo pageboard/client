10e6616cde5318202aabefb192e70bcd8f40bc72
Jérémy Lal <kapouer@melix.org>
2017-09-24 19:35:38 +0200

    A quick Embed element for iframes

---
+++ packages/elements-semantic-ui/elements/embed.js
@@ -0,0 +1,43 @@
+Pageboard.elements.embed = {
+	title: "Embed",
+	properties: {
+		url: {
+			title: 'Address',
+			description: 'The iframe src URL',
+			type: "string",
+			format: "uri"
+			// TODO plug embeds to href, but url-inspector makes it difficult for us right now
+		},
+		placeholder: {
+			title: 'Placeholder',
+			description: 'The iframe previsualisation image',
+			type: "string",
+			format: "uri"
+		},
+		autoPlay: {
+			title: 'Auto play',
+			type: "boolean",
+			default: false
+		}
+	},
+	group: "block",
+	icon: '<i class="external icon"></i>',
+	parse: function(dom) {
+		var attrs = {};
+		if (dom.matches('iframe')) attrs.url = dom.getAttribute('src');
+		return attrs;
+	},
+	tag: 'iframe,element-embed',
+	render: function(doc, block, view) {
+		var d = block.data;
+		var node = doc.dom`<element-embed class="ui embed" data-url="${d.url}" data-auto-play="${!view.editable && d.autoPlay}"></element-embed>`;
+		if (d.placeholder) node.setAttribute('data-placeholder', d.thumbnail);
+		return node;
+	},
+	scripts: ['../ui/element-embed.js'],
+	stylesheets: [
+		'../ui/element-embed.css',
+		'/.pageboard/semantic-ui/components/embed.css'
+	]
+};
+
---
+++ packages/elements-semantic-ui/ui/element-embed.js
@@ -0,0 +1,21 @@
+class HTMLElementEmbed extends HTMLElement {
+	constructor() {
+		super();
+	}
+	connectedCallback() {
+		if (!this.iframe) {
+			this.iframe = this.dom`<iframe src="${this.dataset.url}" width="100%" height="100%" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>`;
+			this.appendChild(this.iframe);
+		}
+	}
+	disconnectedCallback() {
+		if (this.iframe) {
+			this.iframe.remove();
+			delete this.iframe;
+		}
+	}
+}
+
+Page.setup(function() {
+	window.customElements.define('element-embed', HTMLElementEmbed);
+});
