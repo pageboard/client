4f8de746783febef37f898cee374b6730481a7e5
Jérémy Lal <kapouer@melix.org>
2017-10-02 02:13:36 +0200

    Use prettier icon for code

--- packages/elements-semantic-ui/elements/code.js
+++ packages/elements-semantic-ui/elements/code.js
@@ -2,7 +2,7 @@ Pageboard.elements.code = {
 	title: "Literal",
 	inline: true,
 	inplace: true,
-	icon: '<b class="icon">"</b>',
+	icon: '<i class="left quote icon">"</i>',
 	render: function(doc, block) {
 		return doc.dom`<code></code>`;
 	}
