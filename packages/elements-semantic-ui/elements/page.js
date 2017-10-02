ebe7f0922cf0f83db002854550554c7bd7a9d773
Jérémy Lal <kapouer@melix.org>
2017-10-02 13:54:01 +0200

    page are standalone by default

--- packages/elements-semantic-ui/elements/page.js
+++ packages/elements-semantic-ui/elements/page.js
@@ -3,6 +3,7 @@ Pageboard.elements.page = {
 	priority: -100,
 	title: 'Page',
 	group: 'page',
+	standalone: true, // besides site, can be child of zero or more parents
 	properties: {
 		title: {
 			title: 'Title',
