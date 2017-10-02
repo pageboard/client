e589bfd4231e79ef6cfc5b4053c61e7c9387bbc5
Jérémy Lal <kapouer@melix.org>
2017-10-02 02:16:27 +0200

    Hey remove that char

--- packages/elements-semantic-ui/elements/code.js
+++ packages/elements-semantic-ui/elements/code.js
@@ -2,7 +2,7 @@ Pageboard.elements.code = {
 	title: "Literal",
 	inline: true,
 	inplace: true,
-	icon: '<i class="left quote icon">"</i>',
+	icon: '<i class="left quote icon"></i>',
 	render: function(doc, block) {
 		return doc.dom`<code></code>`;
 	}
