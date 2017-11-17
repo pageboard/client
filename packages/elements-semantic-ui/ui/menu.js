319be61d02fe1ed6b74870be399128c7dc0855d6
Jérémy Lal <kapouer@melix.org>
2017-11-17 12:24:25 +0100

    Fix menu item active with safer code

--- packages/elements-semantic-ui/ui/menu.js
+++ packages/elements-semantic-ui/ui/menu.js
@@ -2,7 +2,7 @@ Page.build(function(state) {
 	Array.prototype.forEach.call(
 		document.querySelectorAll('.ui.menu [href]'),
 		function(item) {
-			if (item.host == state.host && item.pathname == state.pathname) {
+			if (Page.sameDomain(item, state) && Page.samePath(item, state)) {
 				item.classList.add('active');
 			}
 		}
