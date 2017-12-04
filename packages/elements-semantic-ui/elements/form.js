7ffad738a33527c4218da36bcb5750d61c169ad2
Jérémy Lal <kapouer@melix.org>
2017-12-04 19:14:17 +0100

    Form select

--- packages/elements-semantic-ui/elements/form.js
+++ packages/elements-semantic-ui/elements/form.js
@@ -252,6 +252,100 @@ Pageboard.elements.input_checkbox = {
 	]
 };
 
+
+Pageboard.elements.input_select = {
+	title: 'Select',
+	menu: "form",
+	required: ["name"],
+	group: 'input',
+	properties: {
+		name: {
+			title: "name",
+			description: "The form object key",
+			type: "string"
+		},
+		placeholder: {
+			title: "placeholder",
+			type: ["string", "null"]
+		},
+		required: {
+			title: 'required',
+			type: 'boolean',
+			default: false
+		},
+		multiple: {
+			title: 'multiple',
+			type: 'boolean',
+			default: false
+		}
+	},
+	contents: {
+		label: {
+			title: 'Label',
+			spec: 'inline*'
+		},
+		options: {
+			title: 'Options',
+			spec: 'input_select_option+'
+		}
+	},
+	icon: '<i class="caret down icon"></i>',
+	render: function(doc, block) {
+		var d = block.data;
+		var select = doc.dom`<element-select class="ui selection dropdown">
+			<div class="menu" block-content="options"></div>
+		</element-select>`;
+
+		if (d.name) select.dataset.name = d.name;
+		if (d.required) {
+			select.dataset.required = true;
+		}
+		if (d.multiple) {
+			select.classList.add('multiple');
+			select.dataset.multiple = true;
+		}
+		if (d.placeholder) {
+			select.dataset.placeholder = d.placeholder;
+		}
+		return doc.dom`<div class="field">
+			<label block-content="label">Label</label>
+			${select}
+		</div>`;
+	},
+	stylesheets: [
+		'/.pageboard/semantic-ui/components/dropdown.css',
+		'/.pageboard/semantic-ui/components/label.css'
+	],
+	scripts: [
+		'../ui/select.js'
+	]
+};
+
+Pageboard.elements.input_select_option = {
+	title: 'Option',
+	menu: "form",
+	properties: {
+		value: {
+			title: "value",
+			description: "Defaults to option label",
+			type: ["string", "null"]
+		}
+	},
+	contents: {
+		label: {
+			title: 'Label',
+			spec: 'inline*'
+		}
+	},
+	render: function(doc, block) {
+		// add selected class if selected ?
+		var node = doc.dom`<div class="item" block-content="label"></div>`;
+		var val = block.data.value;
+		if (val != null) node.dataset.value = val;
+		return node;
+	}
+};
+
 Pageboard.elements.form_message = {
 	title: 'Message',
 	menu: "form",
---
+++ packages/elements-semantic-ui/ui/select.js
@@ -0,0 +1,124 @@
+class HTMLElementSelect extends HTMLElement {
+	static get observedAttributes() {return ['data-placeholder', 'data-name', 'data-multiple']; }
+	constructor() {
+		super();
+		this._click = this._click.bind(this);
+		this._change = this._change.bind(this);
+	}
+	_click(e) {
+		var item = e.target.closest('.item');
+		if (item) {
+			this._selectItem(item);
+			this._selectSelf(true);
+		} else if (e.target.matches('.delete')) {
+			var label = e.target.closest('.label');
+			var val = label.dataset.value;
+			this._deselectItem(val);
+		} else {
+			this._selectSelf(!e.target.closest('element-select'));
+		}
+	}
+	_selectSelf(close) {
+		// toggle menu
+		var menu = this.querySelector('.menu');
+		if (menu.style.display != "block" && !close) menu.style.display = 'block';
+		else menu.style.display = 'none';
+	}
+	_selectItem(item) {
+		var val = item.dataset.value || item.innerText;
+		var str = item.innerHTML;
+		var wasSelected = false;
+		var option = this.querySelector(`select > option[value="${val}"]`);
+		if (option) {
+			if (option.selected) wasSelected = true;
+			option.selected = true;
+		}
+		if (this.dataset.multiple) {
+			if (!wasSelected) this.insertBefore(this.dom`<a class="ui label" data-value="${val}">${str}<i class="delete icon"></i></a>`, this._setText(""));
+		} else {
+			this._setText(str);
+		}
+
+		var defaultOption = this.querySelector(`select > option[value=""]`);
+		if (defaultOption) defaultOption.selected = false;
+	}
+	_deselectItem(val) {
+		if (this.dataset.multiple) {
+			this.querySelector(`[data-value="${val}"]`).remove();
+		}
+		if (!this.dataset.multiple || this.querySelector('.label[data-value]') == null) {
+			this._setPlaceholder();
+		}
+		var option = this.querySelector(`select > option[value="${val}"]`);
+		if (option) option.selected = false;
+	}
+	_setText(str) {
+		var text = this.querySelector('.text');
+		text.textContent = str;
+		text.classList.remove('default');
+		return text;
+	}
+	_setPlaceholder(str) {
+		var text = this.querySelector('.text');
+		text.textContent = "";
+		if (!str) str = this.dataset.placeholder;
+		text.appendChild(document.createTextNode(str || ''));
+		text.classList.add('default');
+
+		var select = this.querySelector('select');
+		select.querySelector('option[value=""]').innerHTML = str || "-";
+	}
+	_change(e) {
+		var items = Array.from(this.querySelector('.menu').children);
+		Array.from(e.target.children).forEach(function(option, i) {
+			var item = items.children[i];
+			if (option.selected) this._selectItem(item);
+		}, this);
+	}
+	_optionItem(item) {
+		return item.dom`<option value="${item.dataset.value || item.innerText}">${item.innerHTML}</option>`;
+	}
+	connectedCallback() {
+		if (!this.querySelector('.icon')) {
+			this.insertBefore(this.dom`<i class="dropdown icon"></i>`, this.firstChild);
+		}
+		var menu = this.querySelector('.menu');
+		var select = this.querySelector('select');
+		if (!select) {
+			select = this.insertBefore(this.dom`<select name="${this.dataset.name}">
+				<option selected value="">-</option>
+				${Array.prototype.map.call(menu.children, this._optionItem)}
+			</select>`, menu);
+		}
+		if (this.dataset.required) select.required = true;
+		if (this.dataset.multiple) select.multiple = true;
+
+		var text = this.querySelector('.text');
+		this.insertBefore(this.dom`<div class="text"></div>`, select);
+		this._setPlaceholder();
+
+		this.ownerDocument.addEventListener('click', this._click, false);
+		select.addEventListener('change', this._change, false);
+	}
+	disconnectedCallback() {
+		this.ownerDocument.removeEventListener('click', this._click, false);
+		var select = this.querySelector('select');
+		if (select) select.removeEventListener('change', this._change, false);
+	}
+	attributeChangedCallback(attributeName, oldValue, newValue, namespace) {
+		var select = this.querySelector('select');
+		if (!select) return;
+		if (attributeName == "data-placeholder" && !select.value) {
+			this._setPlaceholder(newValue);
+		}
+		if (attributeName == "data-name") {
+			select.name = newValue;
+		} else if (attributeName == "data-multiple") {
+			select.multiple = !!newValue;
+		}
+	}
+}
+
+Page.build(function() {
+	window.customElements.define('element-select', HTMLElementSelect);
+});
