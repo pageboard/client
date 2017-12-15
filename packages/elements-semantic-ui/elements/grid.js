2db347d79f55f98e2d2cc70ce21d6a0e460899c3
Jérémy Lal <kapouer@melix.org>
2017-12-15 12:18:09 +0100

    Add row to grid

--- packages/elements-semantic-ui/elements/grid.js
+++ packages/elements-semantic-ui/elements/grid.js
@@ -3,8 +3,8 @@ Pageboard.elements.grid = {
 	group: "block",
 	contents: {
 		columns: {
-			spec: "grid_column+",
-			title: 'columns'
+			spec: "(grid_column|grid_row)+",
+			title: 'cells'
 		}
 	},
 	icon: '<i class="icon grid layout"></i>',
@@ -13,7 +13,26 @@ Pageboard.elements.grid = {
 	},
 	stylesheets: [
 		'/.pageboard/semantic-ui/components/grid.css'
-	]
+	],
+	prefixes: {
+		0: '',
+		1: 'one',
+		2: 'two',
+		3: 'three',
+		4: 'four',
+		5: 'five',
+		6: 'six',
+		7: 'seven',
+		8: 'eight',
+		9: 'nine',
+		10: 'ten',
+		11: 'eleven',
+		12: 'twelve',
+		13: 'thirteen',
+		14: 'fourteen',
+		15: 'fifteen',
+		16: 'sixteen'
+	}
 };
 
 
@@ -38,27 +57,22 @@ Pageboard.elements.grid_column = {
 	icon: '<i class="icon columns"></i>',
 	render: function(doc, block) {
 		var prefix = '';
-		if (block.data.width != null) prefix = {
-			0: '',
-			1: 'one',
-			2: 'two',
-			3: 'three',
-			4: 'four',
-			5: 'five',
-			6: 'six',
-			7: 'seven',
-			8: 'eight',
-			9: 'nine',
-			10: 'ten',
-			11: 'eleven',
-			12: 'twelve',
-			13: 'thirteen',
-			14: 'fourteen',
-			15: 'fifteen',
-			16: 'sixteen'
-		}[block.data.width];
+		if (block.data.width != null) prefix = Pageboard.elements.grid.prefixes[block.data.width];
 		if (prefix) prefix += " wide ";
-		return doc.dom`<div class="${prefix} column" block-content="content"></div>`;
+		return doc.dom`<div class="${prefix}column" block-content="content"></div>`;
 	}
 };
 
+Pageboard.elements.grid_row = {
+	title: "Row",
+	contents: {
+		columns: {
+			spec: "grid_column+",
+			title: 'columns'
+		}
+	},
+	icon: '<b class="icon">row</b>',
+	render: function(doc, block) {
+		return doc.dom`<div class="row" block-content="columns"></div>`;
+	}
+};
