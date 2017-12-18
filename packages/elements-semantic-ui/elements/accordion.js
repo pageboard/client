656042364cf73b9deaaef51994ff061997dedf7a
Jérémy Lal <kapouer@melix.org>
2017-12-18 10:17:48 +0100

    Work around pagecut limitations

--- packages/elements-semantic-ui/elements/accordion.js
+++ packages/elements-semantic-ui/elements/accordion.js
@@ -41,12 +41,12 @@ Pageboard.elements.fold = {
 	},
 	icon: '<i class="caret right icon"></i>',
 	render: function(doc, block) {
-		return doc.dom`<div class="fold">
+		return doc.dom`<div class="fold" contenteditable="false">
 			<div class="title">
 				<i class="dropdown icon"></i>
-				<span block-content="title">Title</span>
+				<span block-content="title" contenteditable="true">Title</span>
 			</div>
-			<div class="content" block-content="content"></div>
+			<div class="content" block-content="content" contenteditable="true"></div>
 		</div>`;
 	}
 };
