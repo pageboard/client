18a516057ae5d93b33d07e5f1b0b6fb83a6a67ac
Jérémy Lal <kapouer@melix.org>
2018-01-08 13:33:41 +0100

    Move placeholder.png to write

--- packages/elements-write/write/crop.js
+++ packages/elements-write/write/crop.js
@@ -208,7 +208,7 @@ Crop.prototype.load = function() {
 
 Crop.prototype.thumbnail = function(url) {
 	if (url) url += '?rs=w:512&q=65';
-	else url = '/.files/@pageboard/elements/ui/placeholder.png';
+	else url = '/.pageboard/write/placeholder.png';
 	return url;
 };
 
