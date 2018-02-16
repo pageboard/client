89ac4531dca2004eb02e56608369733f680a81fb
Jérémy Lal <kapouer@melix.org>
2018-02-16 18:48:08 +0100

    Add translate element

---
+++ packages/elements-google/elements/translate.js
@@ -0,0 +1,34 @@
+Pageboard.elements.google_translate = {
+	title: 'Translate',
+	group: 'menu_item',
+	context: "menu/",
+	menu: 'link',
+	icon: '<i class="google icon"></i>',
+	properties: {
+		lang: {
+			title: 'Page language',
+			type: 'string'
+		},
+		banner: {
+			title: 'Automatic banner',
+			type: 'boolean',
+			default: false
+		}
+	},
+	contents: {
+		title: "inline*"
+	},
+	render: function(doc, block) {
+		var node = doc.dom`<element-google-translate class="item">
+			<div block-content="title"></div>
+		</element-google-translate>`;
+		Object.assign(node.dataset, block.data);
+		return node;
+	},
+	scripts: [
+		'../ui/translate.js'
+	],
+	stylesheets: [
+		'../ui/translate.css'
+	]
+};
---
+++ packages/elements-google/ui/translate.js
@@ -0,0 +1,61 @@
+class HTMLElementGoogleTranslate extends HTMLCustomElement {
+	init() {
+		this.overHandler = this.overHandler.bind(this);
+		this.focusHandler = this.focusHandler.bind(this);
+		this.blurHandler = this.blurHandler.bind(this);
+		this.blurFrame = this.blurFrame.bind(this);
+	}
+	connectedCallback() {
+		var id = this.id || ("id" + Date.now());
+		var cbName = `HTMLElementGoogleTranslate_${id}`;
+		window[cbName] = this.update.bind(this);
+		if (!this._id) {
+			this.id = id;
+			var script = this.ownerDocument.createElement('script');
+			script.src = `https://translate.google.com/translate_a/element.js?cb=${cbName}`;
+			this.ownerDocument.head.appendChild(script);
+		}
+		this.addEventListener('mouseover', this.overHandler, false);
+		this.addEventListener('focus', this.focusHandler, false);
+		this.addEventListener('blur', this.blurHandler, false);
+	}
+	update() {
+		if (document.body.contentEditable == "true") {
+			return;
+		}
+		if (this._translateElement) {
+			return;
+		}
+		var TE = google.translate.TranslateElement;
+		this._translateElement = new TE({
+			pageLanguage: this.dataset.lang || this.ownerDocument.documentElement.lang,
+			layout: TE.InlineLayout.SIMPLE,
+			autoDisplay: this.dataset.banner == "true"
+		}, this.id);
+	}
+	disconnectedCallback() {
+		this.removeElementListener('mouseover', this.overHandler, false);
+		this.removeElementListener('focus', this.focusHandler, false);
+		this.removeElementListener('blur', this.blurHandler, false);
+	}
+	overHandler(e) {
+		this.focusHandler(e);
+	}
+	focusHandler(e) {
+		this.classList.add('active');
+		this.querySelector('.goog-te-gadget-simple').click();
+		var frame = document.querySelector('.goog-te-menu-frame');
+		frame.addEventListener('mouseout', this.blurFrame, false);
+	}
+	blurHandler(e) {
+		this.blurFrame();
+	}
+	blurFrame() {
+		var frame = document.querySelector('.goog-te-menu-frame');
+		frame.blur();
+		frame.removeEventListener('mouseout', this.blurFrame, false);
+		this.classList.remove('active');
+	}
+}
+window.customElements.define('element-google-translate', HTMLElementGoogleTranslate);
+
