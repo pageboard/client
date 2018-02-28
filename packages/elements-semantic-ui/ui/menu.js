02be71f3e6d2af2de15df629f1cf43a4de8bd4b6
Jérémy Lal <kapouer@melix.org>
2018-02-28 10:49:24 +0100

    Do not set active if href attribute is empty

--- packages/elements-semantic-ui/ui/menu.js
+++ packages/elements-semantic-ui/ui/menu.js
@@ -2,7 +2,7 @@ Page.build(function(state) {
 	Array.prototype.forEach.call(
 		document.querySelectorAll('.ui.menu [href]'),
 		function(item) {
-			if (Page.sameDomain(item, state) && Page.samePath(item, state)) {
+			if (item.getAttribute('href') && Page.sameDomain(item, state) && Page.samePath(item, state)) {
 				item.classList.add('active');
 			}
 		}
