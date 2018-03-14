f5386cfd9550980f1bb89b456c706175ff0865f4
Jérémy Lal <kapouer@melix.org>
2018-03-14 15:01:59 +0100

    input-range: use fill/reset and update rangeSlider

--- packages/elements-semantic-ui/ui/input-range.js
+++ packages/elements-semantic-ui/ui/input-range.js
@@ -1,16 +1,35 @@
 class HTMLElementInputRange extends HTMLCustomElement {
 	connectedCallback() {
-		var input = this.querySelector('input');
-		if (!input) return;
-		rangeSlider.create(input);
+		this._input = this.querySelector('input');
+		if (!this._input) return;
+		var me = this;
+		rangeSlider.create(this._input, {
+			onSlideEnd: function(val) {
+				me._input.rangeFill(val);
+				var e = document.createEvent('HTMLEvents');
+				e.initEvent('change', true, true);
+				me._input.dispatchEvent(e);
+			}
+		});
 	}
 
 	disconnectedCallback() {
-		var input = this.querySelector('input');
-		if (!input) return;
-		if (input.rangeSlider) input.rangeSlider.destroy();
+		var input = this._input;
+		if (input && input.rangeSlider) input.rangeSlider.destroy();
 	}
 }
+
+HTMLInputElement.prototype.rangeFill = function(val) {
+	this.value = val;
+	if (this.rangeSlider) this.rangeSlider.update();
+	if (!this.required && val == this.dataset.default) this.disabled = true;
+	else this.disabled = false;
+};
+
+HTMLInputElement.prototype.rangeReset = function() {
+	this.rangeFill(this.dataset.default);
+};
+
 
 Page.setup(function() {
 	HTMLCustomElement.define('element-input-range', HTMLElementInputRange);
