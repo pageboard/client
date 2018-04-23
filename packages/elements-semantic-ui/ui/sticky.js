e0e75219f09225083d194f28f3534c9ce7309ba7
Jérémy Lal <kapouer@melix.org>
2018-04-23 18:43:00 +0200

    Use static init method for Sticky init

--- packages/elements-semantic-ui/ui/sticky.js
+++ packages/elements-semantic-ui/ui/sticky.js
@@ -1,14 +1,22 @@
 class HTMLElementSticky extends HTMLCustomElement {
+	static init() {
+		this.manager = stickybits({
+			stickyClass: 'is-sticky',
+			activeClass: 'is-sticky',
+			parentClass: null,
+			useStickyClasses: true
+		});
+	}
 	setup() {
 		if (!this.parentNode) return;
 		if (this._sticky) this.destroy();
-		this._sticky = HTMLElementSticky.manager.addInstance(this, {
+		this._sticky = this.constructor.manager.addInstance(this, {
 			verticalPosition: this.dataset.position
 		});
 	}
 	destroy() {
 		if (!this._sticky) return;
-		HTMLElementSticky.manager.removeInstance(this._sticky);
+		this.constructor.manager.removeInstance(this._sticky);
 		delete this._sticky;
 	}
 	connectedCallback() {
@@ -24,11 +32,5 @@ class HTMLElementSticky extends HTMLCustomElement {
 }
 
 Page.setup(function() {
-	HTMLElementSticky.manager = stickybits({
-		stickyClass: 'is-sticky',
-		activeClass: 'is-sticky',
-		parentClass: null,
-		useStickyClasses: true
-	});
 	HTMLCustomElement.define('element-sticky', HTMLElementSticky);
 });
