441910770f32794ba0bf0852ecd83e267e396ba8
Jérémy Lal <kapouer@melix.org>
2018-05-05 18:02:25 +0200

    Transitions and async nav ftw

--- packages/elements-semantic-ui/ui/transition.js
+++ packages/elements-semantic-ui/ui/transition.js
@@ -1,17 +1,26 @@
 (function() {
 	var list = ['init', 'route', 'build', 'patch', 'setup', 'close', 'error'];
 	var tokens = document.documentElement.classList;
+	var idTimeout;
 	function pageListener(e) {
 		if (!Page.state) return;
+		if (idTimeout) {
+			clearTimeout(idTimeout);
+			idTimeout = null;
+		}
 		var cur = e.type.substring(4);
 		tokens.add(cur);
 		list.forEach(function(name) {
 			if (name != cur) tokens.remove(name);
 		});
-		if (cur == "setup") {
-			setTimeout(function() {
-				tokens.remove('setup');
-			}, 400);
+		var timeout = 0;
+		if (cur == "setup" || cur == "patch") timeout = 400;
+		if (cur == "error") timeout = 3000;
+		if (timeout) {
+			idTimeout = setTimeout(function() {
+				idTimeout = null;
+				tokens.remove(cur);
+			}, timeout);
 		}
 	}
 	list.forEach(function(name) {
@@ -20,52 +29,35 @@
 })();
 
 Page.setup(function(state) {
-	var tEndC;
-	var tEnd = "transitionend";
-
-	var transitions = {
-		transition: tEnd,
-		OTransition: 'oTransitionEnd',
-		MozTransition: tEnd,
-		msTransition: 'MSTransitionEnd',
-		WebkitTransition: 'webkitTransitionEnd'
-	};
-
-	var st = document.body.style;
-	for (var t in transitions) {
-		if (st[t] !== undefined) tEndC = transitions[t];
-	}
-
-	if (tEndC) {
-		document.body.addEventListener('click', function(e) {
-			var a = e.target.closest('a');
-			var href = a && a.getAttribute('href');
-			if (href) {
-				if (document.body.isContentEditable) {
-					e.preventDefault();
-					return;
-				}
-				if (a.target == "_blank") {
-					return;
-				} else {
-					e.preventDefault();
-					Page.push(href);
-				}
+	document.body.addEventListener('click', function(e) {
+		var a = e.target.closest('a');
+		var href = a && a.getAttribute('href');
+		if (href) {
+			if (document.body.isContentEditable) {
+				e.preventDefault();
+				return;
 			}
-		}, false);
-	} else {
-		return;
-	}
+			if (a.target) {
+				return;
+			} else {
+				e.preventDefault();
+				Page.push(href);
+			}
+		}
+	}, false);
 
 	Page.updateBody = function(body) {
 		var from = document.body.dataset.transitionFrom;
 		var to = body.dataset.transitionTo;
-		if (!from && !to) {
+		var transitionEnd = transitionEndEvent();
+		if (!transitionEnd || !from && !to) {
 			return body;
 		}
+		var doc = document.documentElement;
 		Page.updateAttributes(document.body, body);
 		var clist = document.body.classList;
 		clist.add('transition');
+		doc.classList.add('transition');
 		if (to) {
 			to = to + "-to";
 			clist.add(to);
@@ -88,12 +80,12 @@ Page.setup(function(state) {
 		setTimeout(function() {
 			clist.add('transitioning');
 		});
-		document.documentElement.addEventListener(tEnd, trDone);
+		document.documentElement.addEventListener(transitionEnd, trDone);
 
 		function trDone(e) {
 			// only transitions of body children are considered
 			if (e.target.parentNode != document.body) return;
-			document.documentElement.removeEventListener(tEnd, trDone);
+			document.documentElement.removeEventListener(transitionEnd, trDone);
 			fromList.forEach(function(node) {
 				node.remove();
 			});
@@ -101,9 +93,27 @@ Page.setup(function(state) {
 				node.classList.remove('transition-to');
 			});
 			clist.remove('transition', 'transitioning');
+			doc.classList.remove('transition');
 			if (from) clist.remove(from);
 			if (to) clist.remove(to);
 		}
 	};
+
+	function transitionEndEvent() {
+		var transitions = {
+			transition: "transitionend",
+			OTransition: 'oTransitionEnd',
+			MozTransition: "transitionend",
+			msTransition: 'MSTransitionEnd',
+			WebkitTransition: 'webkitTransitionEnd'
+		};
+
+		var st = document.body.style;
+		for (var t in transitions) {
+			if (st[t] !== undefined) {
+				return transitions[t];
+			}
+		}
+	}
 });
 
