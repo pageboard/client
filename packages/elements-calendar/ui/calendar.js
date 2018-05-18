866544ba437c4976e732088e8db591db439c3bbf
Jérémy Lal <kapouer@melix.org>
2018-05-18 19:05:59 +0200

    byWeek

--- packages/elements-calendar/ui/calendar.js
+++ packages/elements-calendar/ui/calendar.js
@@ -81,3 +81,55 @@ HTMLElementQuery.filters.formatDate = function(val, what, ...list) {
 	var lang = document.documentElement.lang || window.navigator.language;
 	return d.toLocaleString(lang, p);
 };
+
+HTMLElementQuery.filters.byWeek = function(val, what, path) {
+	// val is an array
+	// path accessor to a property
+	// unit is year/month/week/day
+	var data = val;
+	var comps = what.expr.clone().path;
+	var head;
+	while (comps.length) {
+		if (typeof data == "object" && data.length) {
+			break;
+		}
+		head = comps.shift();
+		data = data[head];
+		if (data == null) break;
+	}
+	if (!Array.isArray(data)) {
+		console.warn("Cannot filter groupBy with a value that is not an array");
+	}
+
+	var weeks = {};
+	var arr = [];
+	var aday = 1000 * 60 * 60 * 24;
+	var curWeek;
+	data.forEach(function(item) {
+		var time = (new Date(what.expr.get(item, path))).getTime();
+		if (isNaN(time)) {
+			console.warn("byWeek filter cannot find a valid date at", path, "in", item);
+			return;
+		}
+		var d = new Date(Math.round(time / aday) * aday);
+		var mon = new Date(d);
+		mon.setDate(d.getDate() - (d.getDay() + 6) % 7);
+		var sun = new Date(mon);
+		sun.setDate(mon.getDate() + 6);
+		var h = mon.getTime().toString();
+		var week = weeks[h];
+		if (!week) {
+			week = weeks[h] = {
+				start: mon.toISOString(),
+				end: sun.toISOString(),
+				list: []
+			};
+			arr.push(week);
+		}
+		week.list.push(item);
+	});
+	// mutate val on purpose
+	while (val.length) val.pop();
+	while (arr.length) val.push(arr.shift());
+};
+
