82ca2d6f36f411c631119474a965518e0d4078de
Jérémy Lal <kapouer@melix.org>
2018-05-25 12:08:21 +0200

    Use Pageboard.site api

--- packages/elements-google/elements/analytics.js
+++ packages/elements-google/elements/analytics.js
@@ -6,8 +6,8 @@ Pageboard.elements.site.properties.ga_tracking_id = {
 
 Pageboard.elements.google_analytics = {
 	install: function(doc, page) {
-		var gaid = page.site.ga_tracking_id;
-		if (!gaid || page.site.env != "production") return;
+		var gaid = Pageboard.site.ga_tracking_id;
+		if (!gaid || Pageboard.site.env != "production") return;
 		// FIXME we do not have a way to tell page.render we want an async script here
 		doc.head.insertAdjacentHTML('beforeEnd', `
 	<script async src="https://www.googletagmanager.com/gtag/js?id=${gaid}" id="gtag"></script>`);
