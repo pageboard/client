30eefd9bcc94dc5b457ab1b424987992e8db931e
Jérémy Lal <kapouer@melix.org>
2018-05-30 02:15:26 +0200

    rating: template

--- packages/elements-semantic-ui/elements/rating.js
+++ packages/elements-semantic-ui/elements/rating.js
@@ -14,14 +14,22 @@ Pageboard.elements.rating = {
 			title: 'Maximum',
 			type: 'integer',
 			default: 4
+		},
+		template: {
+			title: 'Template',
+			description: 'Query template',
+			type: 'string',
+			context: 'query|form'
 		}
 	},
 	render: function(doc, block) {
 		var d = block.data;
-		return doc.dom`<div class="ui star rating">${
+		var node = doc.dom`<div class="ui star rating">${
 			(new Array(d.value + 1)).join('<i class="icon active"></i>') +
 			(new Array(d.maximum + 1)).join('<i class="icon"></i>')
 		}</div>`;
+		if (d.template) node.dataset.value = d.template;
+		return node;
 	},
 	stylesheets: [
 		'../semantic-ui/rating.css'
