16ce5e6c5143789f0e0cae00b8384bf6c44b91ce
Jérémy Lal <kapouer@melix.org>
2018-06-12 16:07:00 +0200

    Overall rewrite of translate display

--- packages/elements-google/ui/translate.js
+++ packages/elements-google/ui/translate.js
@@ -1,4 +1,11 @@
 class HTMLElementGoogleTranslate extends HTMLCustomElement {
+	init() {
+		this.trackPosition = this.trackPosition.bind(this);
+	}
+	raf(cb) {
+		if (window.requestAnimationFrame) window.requestAnimationFrame(cb);
+		else setTimeout(cb, 100);
+	}
 	connectedCallback() {
 		var id = this.id || ("id" + Date.now());
 		var cbName = `HTMLElementGoogleTranslate_${id}`;
@@ -6,25 +13,6 @@ class HTMLElementGoogleTranslate extends HTMLCustomElement {
 		if (!this.id) {
 			this.id = id;
 			var script = this.ownerDocument.createElement('script');
-			var me = this;
-			if (this.dataset.opened == "true") {
-				document.body.classList.add('google-translate-keep');
-				script.onload = function() {
-					function checkLoaded() {
-						setTimeout(function() {
-							var frame = document.querySelector('body > iframe.goog-te-menu-frame');
-							if (frame) {
-								me.open();
-								if (frame.style.display != "none") return;
-							}
-							checkLoaded();
-						}, 50);
-					}
-					checkLoaded();
-				};
-			} else {
-				document.body.classList.remove('google-translate-keep');
-			}
 			script.src = `https://translate.google.com/translate_a/element.js?cb=${cbName}`;
 			this.ownerDocument.head.appendChild(script);
 		}
@@ -36,15 +24,22 @@ class HTMLElementGoogleTranslate extends HTMLCustomElement {
 		if (document.body.contentEditable == "true") {
 			return;
 		}
-		if (this._translateElement) {
-			return;
+		if (!this._translateElement) {
+			var TE = google.translate.TranslateElement;
+			this._translateElement = new TE({
+				pageLanguage: this.ownerDocument.documentElement.lang,
+				layout: TE.InlineLayout.SIMPLE,
+				autoDisplay: this.dataset.banner == "true"
+			}, this.id);
 		}
-		var TE = google.translate.TranslateElement;
-		this._translateElement = new TE({
-			pageLanguage: this.ownerDocument.documentElement.lang,
-			layout: TE.InlineLayout.SIMPLE,
-			autoDisplay: this.dataset.banner == "true"
-		}, this.id);
+		if (this.dataset.opened == "true") {
+			document.body.classList.add('google-translate-keep');
+			this.classList.add('active');
+			this.trackPosition();
+		} else {
+			this.classList.remove('active');
+			document.body.classList.remove('google-translate-keep');
+		}
 	}
 	disconnectedCallback() {
 		this.removeEventListener('mouseover', this.overHandler, false);
@@ -72,7 +67,23 @@ class HTMLElementGoogleTranslate extends HTMLCustomElement {
 		}
 		this.classList.remove('active');
 	}
+	clickHandler() {
+		this.position();
+	}
+	trackPosition() {
+		if (this.dataset.opened != "true") return;
+		var frame = document.querySelector('body > iframe.goog-te-menu-frame');
+		if (frame) {
+			var rect = this.getBoundingClientRect();
+			var top = rect.y + Math.round(rect.height) + 'px';
+			var left = rect.x + 'px';
+			if (frame.style.top != top) frame.style.top = top;
+			if (frame.style.left != left) frame.style.left = left;
+		}
+		this.raf(this.trackPosition);
+	}
 	open() {
+		if (this.classList.contains('active') && this.dataset.opened == "true") return;
 		this.classList.add('active');
 		var it = this.querySelector('.goog-te-gadget-simple');
 		if (it) it.click();
