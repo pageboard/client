a429938184f109345fe3fa2484acdf462f97d0c0
Jérémy Lal <kapouer@melix.org>
2018-06-19 15:34:36 +0200

    polyfill element is esssential

---
+++ packages/elements/elements/polyfill.js
@@ -0,0 +1,21 @@
+Pageboard.elements.polyfill = {
+	priority: -101, // before page
+	url: 'https://cdn.polyfill.io/v2/polyfill.min.js',
+	install: function(doc) {
+		var map = {};
+		Object.keys(Pageboard.elements).forEach(function(key) {
+			var list = Pageboard.elements[key].polyfills;
+			if (!list) return;
+			if (typeof list == "string") list = [list];
+			list.forEach(function(item) {
+				map[item] = true;
+			});
+		});
+		var features = Object.keys(map).join(',');
+		if (features) {
+			Pageboard.elements.page.scripts.unshift(`${this.url}?flags=gated&features=${features}`);
+		}
+	},
+	polyfills: 'default'
+};
+
