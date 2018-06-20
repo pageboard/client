8f2a1df12b2da72ac847a05dbee865d11bbe6233
Jérémy Lal <kapouer@melix.org>
2018-06-20 16:10:28 +0200

    Only on home page

--- packages/elements-google/elements/verification.js
+++ packages/elements-google/elements/verification.js
@@ -5,6 +5,7 @@ Pageboard.elements.site.properties.google_site_verification = {
 
 Pageboard.elements.google_site_verification = {
 	install: function(doc, page) {
+		if (page.data.url != "/") return;
 		var id = Pageboard.site.google_site_verification;
 		if (!id || Pageboard.site.env != "production") return;
 		doc.head.appendChild(doc.dom`<meta name="google-site-verification" content="${id}">`);
