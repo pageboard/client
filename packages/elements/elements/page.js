f92f4bb0403f6014ac66c70fe3d05a31a2f7f681
Jérémy Lal <kapouer@melix.org>
2018-06-23 02:05:07 +0200

    notfound need separate render

--- packages/elements/elements/page.js
+++ packages/elements/elements/page.js
@@ -119,8 +119,9 @@ Pageboard.elements.notfound = Object.assign({}, Pageboard.elements.page, {
 	properties: Object.assign({}, Pageboard.elements.page.properties),
 	render: function(doc, block, view) {
 		doc.head.appendChild(doc.dom`<meta http-equiv="Status" content="404 Not Found">`);
-		return Pageboard.elements.page.render(doc, block, view);
+		return this.pageRender(doc, block, view);
 	}
 });
+Pageboard.elements.notfound.pageRender = Pageboard.elements.page.render;
 delete Pageboard.elements.notfound.properties.url;
 
