07ce98ef9cb4095c30e430669aa728033db8b4ec
Jérémy Lal <kapouer@melix.org>
2018-06-28 18:22:01 +0200

    Move query, query_template, query_message to core

--- packages/elements-semantic-ui/elements/query.js
+++ packages/elements-semantic-ui/elements/query.js
@@ -1,137 +1,19 @@
-Pageboard.elements.query = {
-	title: "Query",
-	menu: "form",
-	priority: 1, // scripts must run after 'form' scripts
-	contents: {
-		messages: {
-			title: 'Messages',
-			spec: '(paragraph|query_message)+'
-		},
-		template: {
-			title: 'Template',
-			spec: 'block+'
-		}
-	},
-	group: "block",
-	icon: '<i class="search icon"></i>',
-	render: function(doc, block) {
-		var node = doc.dom`<element-query class="ui form">
-			<div>
-				<div block-content="messages"></div>
-				<div block-content="template"></div>
-			</div>
-			<div class="results"></div>
-		</element-query>`;
-		var d = block.data;
-		if (d.query) {
-			node.dataset.nocall = !d.query.call;
-			if (d.query.type) {
-				node.dataset.type = d.query.type;
-			}
-			if (d.query.vars) {
-				node.dataset.vars = Object.keys(d.query.vars).map(function(key) {
-					return d.query.vars[key];
-				}).join(',');
-			}
-		}
-		return node;
-	},
-	required: ["query"],
-	properties: {
-		query: {
-			title: 'Query',
-			type: 'object',
-			properties: {
-				call: {
-					title: 'Call service',
-					description: 'Can be empty to merge only url query',
-					anyOf: [{
-						type: 'null'
-					}, {
-						type: "string",
-						pattern: "^\\w+\.\\w+$"
-					}],
-					input: {
-					}
-				},
-				type: {
-					title: 'Which element',
-					description: 'Checks query against schema',
-					type: ['null', 'string'],
-					input: {
-						name: 'element',
-						properties: true
-					}
-				},
-				consts: {
-					title: 'Constants',
-					description: 'list of path.to.key -> value',
-					anyOf: [{
-						type: "object"
-					}, {
-						type: "null"
-					}]
-				},
-				vars: {
-					title: 'Variables',
-					description: "list of path.to.key -> query.key",
-					anyOf: [{
-						type: "object"
-					}, {
-						type: "null"
-					}]
-				}
-			}
-		}
-	},
-	stylesheets: [
-		'../ui/query.css'
-	],
-	scripts: [
-		'../lib/matchdom.js',
-		'../ui/query.js'
-	],
-	polyfills: [
-		'fetch'
-	]
+Pageboard.elements.query._render = Pageboard.elements.query.render;
+Pageboard.elements.query.render = function(doc, block) {
+	var node = Pageboard.elements.query._render(doc, block);
+	node.classList.add('ui', 'form');
+	return node;
 };
 
-Pageboard.elements.query_message = {
-	title: 'Message',
-	menu: "form",
-	group: "block",
-	context: 'query//',
-	properties: {
-		type: {
-			title: "type",
-			description: "Message is shown depending on type",
-			default: "success",
-			anyOf: [{
-				const: "success",
-				title: "Success"
-			}, {
-				const: "warning",
-				title: "Not found"
-			}, {
-				const: "error",
-				title: "Error"
-			}]
-		}
-	},
-	contents: {
-		message: {
-			title: 'Message',
-			spec: "block+"
-		}
-	},
-	icon: '<i class="announcement icon"></i>',
-	render: function(doc, block) {
-		return doc.dom`<div class="ui message ${block.data.type}" block-content="message">Message</div>`
-	},
-	stylesheets: [
-		'../semantic-ui/message.css'
-	]
+Pageboard.elements.query_message._render = Pageboard.elements.query_message.render;
+Pageboard.elements.query_message.render = function(doc, block) {
+	var node = Pageboard.elements.query_message._render(doc, block);
+	node.classList.add('ui', 'message');
+	return node;
 };
+Pageboard.elements.query_message.stylesheets.push(
+	'../semantic-ui/message.css'
+);
 
 Pageboard.elements.query_tags = {
 	title: 'Tags',
@@ -148,48 +30,11 @@ Pageboard.elements.query_tags = {
 		</element-query-tags>`
 	},
 	stylesheets: [
-		'../semantic-ui/label.css'
+		'../semantic-ui/label.css',
+		'../ui/query-tags.css'
 	],
 	scripts: [
-		'../ui/query.js'
+		'../ui/query-tags.js'
 	]
 };
 
-Pageboard.elements.query_template = {
-	title: "Template",
-	properties: {
-		fill: {
-			title: 'Fill',
-			description: 'fill content with matchdom expression, filters on new lines',
-			type: 'string',
-			input: {
-				multiline: true
-			}
-		},
-		attr: {
-			title: 'Attribute',
-			description: 'set attributes with matchdom expression, filters on new lines',
-			type: 'string',
-			input: {
-				multiline: true
-			}
-		},
-		placeholder: {
-			title: 'Placeholder',
-			type: 'string'
-		}
-	},
-	context: 'query//',
-	inline: true,
-	group: "inline",
-	icon: '<b class="icon">var</b>',
-	render: function(doc, block) {
-		var d = block.data;
-		var fill = (d.fill || '').trim().split('\n').join('|');
-		var ph = d.placeholder || fill.split('|', 1)[0].split('.').pop();
-		var node = doc.dom`<span>${ph || '-'}</span>`;
-		if (d.attr) node.dataset.attr = `[${d.attr.trim().split('\n').join('|')}]`;
-		if (fill) node.dataset.fill = `[${fill}|fill]`;
-		return node;
-	}
-};
---
+++ packages/elements-semantic-ui/ui/query-tags.js
@@ -0,0 +1,59 @@
+class HTMLElementQueryTags extends HTMLCustomElement {
+	init() {
+		this.close = this.close.bind(this);
+	}
+	connectedCallback() {
+		this.addEventListener('click', this.close);
+		if (this.children.length) this.refresh();
+	}
+	disconnectedCallback() {
+		this.removeEventListener('click', this.close);
+	}
+	refresh(query) {
+		if (!query) {
+			if (!Page.state) return;
+			query = Page.state.query;
+		}
+		var labels = this.querySelector('.labels');
+		if (!labels) return;
+		labels.textContent = '';
+		var control, field, label;
+		for (var name in query) {
+			HTMLElementQuery.find(name, query[name]).forEach(function(control) {
+				if (control.type == "hidden") return;
+				field = control.closest('.field');
+				if (!field) return;
+				label = field.querySelector('label');
+				if (!label) return;
+				if (control.value == "") return;
+				labels.insertAdjacentHTML('beforeEnd', `<a class="ui label" data-name="${name}" data-value="${control.value}">
+					${label.innerText}
+					<i class="delete icon"></i>
+				</a>`);
+			}, this);
+		}
+	}
+	close(e) {
+		var label = e.target.closest('.label');
+		if (!label) return;
+		HTMLElementQuery.find(label.dataset.name, label.dataset.value).forEach(function(control) {
+			if (control.type == "hidden") return;
+			if (control.checked) control.checked = false;
+			else if (control.reset) control.reset();
+			else if (control.value) control.value = "";
+			var e = document.createEvent('HTMLEvents');
+			e.initEvent('submit', true, true);
+			control.form.dispatchEvent(e);
+		}, this);
+		label.remove();
+	}
+}
+
+HTMLCustomElement.define('element-query-tags', HTMLElementQueryTags);
+
+Page.patch(function(state) {
+	Array.from(document.querySelectorAll('element-query-tags')).forEach(function(node) {
+		node.refresh(state.query);
+	});
+});
+
