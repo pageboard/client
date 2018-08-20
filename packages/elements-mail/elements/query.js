4a84bb1e5d6b7459324ae068c84f101435ecfc03
Jérémy Lal <kapouer@melix.org>
2018-08-21 00:15:32 +0200

    Update mail_query to match new query

--- packages/elements-mail/elements/query.js
+++ packages/elements-mail/elements/query.js
@@ -1,16 +1,17 @@
 Pageboard.elements.mail_query = Object.assign({}, Pageboard.elements.query, {
+	group: "mail_block",
 	contents: {
 		template: {
 			title: 'Template',
 			spec: 'mail_block+'
+		},
+		view: {
+			title: 'View',
+			spec: 'mail_block+',
+			virtual: true
 		}
-	},
-	group: "mail_block"
+	}
 });
-Pageboard.elements.mail_query.fuse = function(node, d) {
-	node.fuse(d);
-	node.querySelector('[block-content="messages"]').remove();
-};
 
 Pageboard.elements.mail_query_template = Object.assign({}, Pageboard.elements.query_template, {
 	context: 'mail_query//',
