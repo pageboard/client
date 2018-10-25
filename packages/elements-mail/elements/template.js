4dc3090c851b54052f66533218b6b3ff7f47d15d
Jérémy Lal <kapouer@melix.org>
2018-10-25 17:11:45 +0200

    query becomes fetch

---
+++ packages/elements-mail/elements/template.js
@@ -0,0 +1,20 @@
+Pageboard.elements.mail_template = Object.assign({}, Pageboard.elements.template, {
+	group: "mail_block mail_template",
+	contents: {
+		template: {
+			title: 'Template',
+			spec: 'mail_block+'
+		}
+	}
+});
+
+Pageboard.elements.mail_fetch = Object.assign({}, Pageboard.elements.fetch, {
+	group: Pageboard.elements.mail_template.group,
+	contents: Pageboard.elements.mail_template.contents
+});
+
+Pageboard.elements.mail_binding = Object.assign({}, Pageboard.elements.binding, {
+	context: 'mail_template//',
+	group: "mail_inline"
+});
+
