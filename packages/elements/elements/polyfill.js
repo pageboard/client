bc455e27532927713daa526fa1c666451543631e
Jérémy Lal <kapouer@melix.org>
2018-10-25 14:17:06 +0200

    Update polyfill

--- packages/elements/elements/polyfill.js
+++ packages/elements/elements/polyfill.js
@@ -1,20 +1,20 @@
 Pageboard.elements.polyfill = {
-	priority: -101, // before page
-	url: 'https://cdn.polyfill.io/v2/polyfill.min.js',
-	install: function(doc, page) {
+	group: "block",
+	html: '<script defer src="https://cdn.polyfill.io/v2/polyfill.min.js?flags=gated&features=[features|url]"></script>',
+	install: function(scope) {
 		var map = {};
-		Object.keys(Pageboard.elements).forEach(function(key) {
-			var list = Pageboard.elements[key].polyfills;
+		Object.keys(scope.$elements).forEach(function(key) {
+			var list = scope.$elements[key].polyfills;
 			if (!list) return;
 			if (typeof list == "string") list = [list];
 			list.forEach(function(item) {
 				map[item] = true;
 			});
 		});
-		var features = Object.keys(map).join(',');
-		if (features) {
-			Pageboard.elements[page.type].scripts.unshift(`${this.url}?flags=gated&features=${features}`);
-		}
+		var pageEl = scope.$elements[scope.$page.type];
+		pageEl.dom.querySelector('head > script').before(this.dom.fuse({
+			features: Object.keys(map).join(',')
+		}));
 	},
 	polyfills: 'default'
 };
