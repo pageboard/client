1b3adb8e4c476180a33fc72b4c96b55ae66ed607
Jérémy Lal <kapouer@melix.org>
2018-10-25 17:11:31 +0200

    Site title in page title

--- packages/elements-mail/elements/mail.js
+++ packages/elements-mail/elements/mail.js
@@ -31,7 +31,7 @@ Pageboard.elements.mail = {
 	},
 	html: `<html lang="[$site.lang]">
 	<head>
-		<title>[title]</title>
+		<title>[$site.title|post:%3A ][title]</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="[$elements.mail.stylesheets|repeat]" />
 		<script defer src="[$elements.mail.scripts|repeat]"></script>
