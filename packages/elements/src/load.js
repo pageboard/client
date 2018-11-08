1c3772f102f161a16c74199f14cb1030e57aa0ea
Jérémy Lal <kapouer@melix.org>
2018-11-08 01:05:37 +0100

    Factor a bit Pageboard.load

--- packages/elements/src/load.js
+++ packages/elements/src/load.js
@@ -1,20 +1,19 @@
-function load(node, head, url) {
-	var doc = node.ownerDocument;
-	var live = doc == document;
+function load(node, head) {
+	var live = node.ownerDocument == document;
 	return new Promise(function(resolve, reject) {
 		if (live) {
 			node.addEventListener('load', function() {
 				resolve();
 			});
 			node.addEventListener('error', function() {
-				var err = new Error(`Cannot load ${url}`);
+				var err = new Error(`Cannot load ${node.src || node.href}`);
 				err.code = 404;
 				reject(err);
 			});
 		}
 		var cursel = node.tagName == "LINK" ? 'script' : 'script:nth-last-child(1) + *';
-		var cursor = doc.head.querySelector(cursel);
-		doc.head.insertBefore(node, cursor);
+		var cursor = head.querySelector(cursel);
+		head.insertBefore(node, cursor);
 		if (!live) resolve();
 	});
 }
@@ -38,25 +37,33 @@ exports.meta = function(meta) {
 	return cache[meta.elements];
 };
 
-exports.js = function(url, doc) {
+function getHead(doc) {
 	if (!doc) doc = document;
-	if (doc.head.querySelector(`script[src="${url}"]`)) {
+	else if (doc.$element) doc = doc.$element.dom;
+	return doc.querySelector('head');
+}
+
+exports.js = function(url, doc) {
+	var head = getHead(doc);
+	doc = head.ownerDocument;
+	if (head.querySelector(`script[src="${url}"]`)) {
 		return Promise.resolve();
 	}
 	var node = doc.createElement('script');
 	node.src = url;
 	node.defer = true;
-	return load(node, url);
+	return load(node, head);
 };
 
 exports.css = function(url, doc) {
-	if (!doc) doc = document;
-	if (doc.head.querySelector(`link[rel="stylesheet"][href="${url}"]`)) {
+	var head = getHead(doc);
+	doc = head.ownerDocument;
+	if (head.querySelector(`link[rel="stylesheet"][href="${url}"]`)) {
 		return Promise.resolve();
 	}
 	var node = doc.createElement('link');
 	node.rel = "stylesheet";
 	node.href = url;
-	return load(node, url);
+	return load(node, head);
 };
 
