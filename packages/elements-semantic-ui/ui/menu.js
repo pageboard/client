35cf84af7a290206294918c1162ec5e1470e5248
Jérémy Lal <kapouer@melix.org>
2018-11-22 09:32:20 +0100

    menu: reselect without develop

--- packages/elements-semantic-ui/ui/menu.js
+++ packages/elements-semantic-ui/ui/menu.js
@@ -2,8 +2,13 @@ Page.patch(function(state) {
 	Array.prototype.forEach.call(
 		document.querySelectorAll('.ui.menu [href]'),
 		function(item) {
-			if (item.getAttribute('href') && Page.samePath(item, state)) {
-				item.classList.add('active');
+			var loc = item.getAttribute('href');
+			if (loc) {
+				loc = Page.parse(loc);
+				if (state.query.develop !== undefined) loc.query.develop = state.query.develop;
+				if (Page.samePath(loc, state)) {
+					item.classList.add('active');
+				}
 			}
 		}
 	);
