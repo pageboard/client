8b8469a834d80e9cc6693bc967bc801ebfd77067
Jérémy Lal <kapouer@melix.org>
2018-12-21 15:25:05 +0100

    write: input element accept "contentless" option

--- packages/elements-write/ui/inputs/element.js
+++ packages/elements-write/ui/inputs/element.js
@@ -5,7 +5,14 @@ function ElementFilter(key, opts, schema) {
 	delete schema.type;
 	schema.anyOf = [{type: 'null', title: 'none'}];
 	Object.values(Pageboard.editor.elements).forEach(function(el) {
-		if (opts.standalone && !el.standalone || el.inplace || !el.title) return;
+		if (!el.title || el.inplace) return;
+		if (opts.contentless && !el.dom) {
+			// keep it
+		} else if (opts.standalone && el.standalone) {
+			// keep it
+		} else {
+			return;
+		}
 		schema.anyOf.push({
 			const: el.name,
 			title: el.title
