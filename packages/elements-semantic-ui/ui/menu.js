516cb338a7f51de3ded25947dc20c39e2a211990
Jérémy Lal <kapouer@melix.org>
2019-02-05 16:19:45 +0100

    ui menu: stop adding back the develop query parameter here

--- packages/elements-semantic-ui/ui/menu.js
+++ packages/elements-semantic-ui/ui/menu.js
@@ -5,7 +5,6 @@ Page.patch(function(state) {
 			var loc = item.getAttribute('href');
 			if (loc) {
 				loc = Page.parse(loc);
-				if (state.query.develop !== undefined) loc.query.develop = state.query.develop;
 				if (Page.samePath(loc, state)) {
 					item.classList.add('active');
 				}
