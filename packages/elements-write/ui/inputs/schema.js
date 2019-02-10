694c2b5156ec4dbaaa017f42cd68bc237708f487
Jérémy Lal <kapouer@melix.org>
2019-02-10 14:07:42 +0100

    write: schema filter can replace a key

--- packages/elements-write/ui/inputs/schema.js
+++ packages/elements-write/ui/inputs/schema.js
@@ -3,7 +3,8 @@ Pageboard.schemaFilters.schema = SchemaFilter;
 /*
 $filter: {
 	name: 'schema',
-	path: 'settings.properties.grants'
+	from: 'settings.properties.grants.items.anyOf'
+	to: 'anyOf'
 }
 will replace current schema with the one given in path
 */
@@ -13,19 +14,23 @@ function SchemaFilter(key, opts) {
 }
 
 SchemaFilter.prototype.update = function(block, schema) {
-	if (!this.opts.path) {
+	if (!this.opts.from) {
 		console.warn("$filter schema is missing path option");
 		return;
 	}
-	var schemaPath = this.opts.path.split('.');
+	var schemaPath = this.opts.from.split('.');
 	var otherSchema = schemaPath.reduce(function(obj, name) {
 		return obj[name] || null;
 	}, Pageboard.editor.elements[schemaPath.shift()]);
 	if (!otherSchema) {
-		console.warn('$filter schema does not find', this.opts.path);
+		console.warn('$filter schema does not find', this.opts.from);
 		return;
 	}
-	return Object.assign({}, otherSchema);
+	if (this.opts.to) {
+		schema[this.opts.to] = otherSchema;
+	} else {
+		return Object.assign({}, otherSchema);
+	}
 };
 
 })(window.Pageboard);
