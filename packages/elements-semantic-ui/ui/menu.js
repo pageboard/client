5e35cb59109ca28d1b048c178f547212ce684bb2
Jérémy Lal <kapouer@melix.org>
2019-04-11 11:36:53 +0200

    menu: mark parent title as active and set default style

--- packages/elements-semantic-ui/ui/menu.js
+++ packages/elements-semantic-ui/ui/menu.js
@@ -4,6 +4,12 @@ Page.patch(function(state) {
 		function(item) {
 			if (item.getAttribute('href') && Page.sameDomain(item, state) && Page.samePath(item, state)) {
 				item.classList.add('active');
+				var ancestor = item;
+				var title;
+				while ((ancestor = ancestor.parentNode.closest('.item'))) {
+					title = ancestor.firstElementChild;
+					if (title && title.matches('.title')) title.classList.add('active');
+				}
 			}
 		}
 	);
