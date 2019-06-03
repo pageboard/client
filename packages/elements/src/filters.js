be7de42dc40b84d74961cdfcc9b08641ce89c3cf
Jérémy Lal <kapouer@melix.org>
2019-06-03 09:35:49 +0200

    Date filters not needing polyfill go to core

--- packages/elements/src/filters.js
+++ packages/elements/src/filters.js
@@ -144,3 +144,69 @@ exports.query = function(obj, what) {
 	if (str) return '?' + str;
 	else return '';
 };
+
+exports.isoDate = function(val, what) {
+	var d = exports.parseDate(val);
+	if (isNaN(d.getTime())) return null;
+	else return d.toISOString();
+};
+
+exports.parseDate = function(val) {
+	var d;
+	if (val instanceof Date) {
+		d = val;
+	} else {
+		if (!val) val = exports.toDate(new Date());
+		else if (/^\d\d:\d\d/.test(val)) {
+			val = '0 ' + val;
+		}
+		d = new Date(val);
+	}
+	return d;
+};
+
+exports.orNow = exports.now = function(val, what) {
+	if (val == null) return Date.now();
+	else return val;
+};
+
+exports.toTime = function(val) {
+	if (!val) return val;
+	return exports.parseDate(val).toISOString().split('T').pop().split('.').shift();
+};
+
+exports.toDate = function(val, what, unit) {
+	if (!val) return val;
+
+	var date = exports.parseDate(val).toISOString().split('T');
+	var time = date.pop().split('.')[0];
+	date = date[0];
+	if (!unit) return date;
+	var parts = date.split('-');
+	if (unit == "year") date = parts[0];
+	else if (unit == "month") date = parts[0] + "-" + parts[1];
+	else if (unit == "time") date = time;
+	else if (unit == "datetime") date += " " + time;
+	return date;
+};
+
+exports.setDate = function(val, what, amount, unit) {
+	var d = exports.parseDate(val);
+	amount = parseInt(amount);
+	if (!isNaN(amount)) {
+		if (!unit) unit = 'day';
+		else unit = unit.toLowerCase();
+		if (unit.endsWith('s')) unit = unit.slice(0, -1);
+		var name = {
+			day: 'Date',
+			month: 'Month',
+			year: 'FullYear',
+			hour: 'Hours',
+			minute: 'Minutes',
+			second: 'Seconds'
+		}[unit];
+		if (!name) throw new Error("Unknown modDate unit " + unit);
+		d[`setUTC${name}`](d[`getUTC${name}`]() + amount);
+	}
+	return d;
+};
