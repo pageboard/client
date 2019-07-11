1200d119205a1fc67b62948c29ea7fb8438985d8
Jérémy Lal <kapouer@melix.org>
2019-07-11 11:29:15 +0200

    embed: allow autoplay

--- packages/elements-semantic-ui/ui/embed.js
+++ packages/elements-semantic-ui/ui/embed.js
@@ -15,7 +15,7 @@ class HTMLElementEmbed extends HTMLCustomElement {
 		}
 		this.iframe = this.firstElementChild;
 		if (!this.iframe) {
-			this.innerHTML = `<iframe src="${src}" width="100%" height="100%" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>`;
+			this.innerHTML = `<iframe src="${src}" width="100%" height="100%" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen allow="autoplay; fullscreen"></iframe>`;
 			this.iframe = this.firstElementChild;
 		} else {
 			this.iframe.setAttribute('src', src);
