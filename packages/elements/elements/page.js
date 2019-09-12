92047c3ce97ad64d226328ae650fc2d85828161d
Jérémy Lal <kapouer@melix.org>
2019-09-12 16:34:18 +0200

    core: fuse polyfills

--- packages/elements/elements/page.js
+++ packages/elements/elements/page.js
@@ -108,6 +108,8 @@ exports.page = {
 				if (!list) return;
 				if (typeof list == "string") list = [list];
 				list.forEach(function(item) {
+					// what.scope from matchdom is not like scope from pageboard
+					item = item.fuse({}, what.scope.data);
 					map[item] = true;
 				});
 			});
