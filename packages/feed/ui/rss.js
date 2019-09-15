b0869fb48bbb6d914c7868b35276034fafe270ea
Jérémy Lal <kapouer@melix.org>
2019-09-16 00:45:06 +0200

    feed: ensure link is inserted once

--- packages/feed/ui/rss.js
+++ packages/feed/ui/rss.js
@@ -2,11 +2,14 @@ Page.patch(function(state) {
 	state.finish(function(state) {
 		var feeds = document.body.querySelectorAll('[block-type="feed"]');
 		if (feeds.length <= 1 || state.pathname.endsWith('.rss')) return;
-		var node = document.head.querySelector('link,script');
 		var rss = Page.format({
 			pathname: state.pathname + '.rss',
 			query: state.query
 		});
-		node.before(node.dom(`<link rel="alternate" type="application/rss+xml" title="${state.scope.$page.data.title}" href="${rss}">`));
+		var node = document.head.querySelector('link[rel="alternate"][type="application/rss+xml"]');
+		if (!node) {
+			var cur = document.head.querySelector('link,script');
+			cur.before(cur.dom(`<link rel="alternate" type="application/rss+xml" title="${state.scope.$page.data.title}" href="${rss}">`));
+		}
 	});
 });
