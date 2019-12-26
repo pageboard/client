86c261cfc8ebc0ca41d9380884cc4989349c8edf
Jérémy Lal <kapouer@melix.org>
2019-12-26 02:13:39 +0100

    html: embed need consent

--- packages/html/ui/embed.js
+++ packages/html/ui/embed.js
@@ -6,22 +6,32 @@ class HTMLElementEmbed extends HTMLCustomElement {
 			loading: "lazy"
 		};
 	}
-	reveal() {
-		var opts = this.options;
-		var src = opts.src;
-		if (opts.hash) {
-			var obj = Page.parse(src);
-			obj.hash = opts.hash;
-			src = Page.format(obj);
-		}
-		this.currentSrc = src;
-		this.iframe = this.firstElementChild;
-		if (!this.iframe) {
-			this.innerHTML = `<iframe width="100%" height="100%" frameborder="0" scrolling="no" webkitAllowFullScreen mozallowfullscreen allowFullScreen allow="autoplay; fullscreen"></iframe>`;
+	reveal(state) {
+		this.classList.add('waiting');
+		state.chain('consent', (state) => {
+			this.classList.remove('waiting', 'denied');
+			var opts = this.options;
+			var src = opts.src;
+			if (opts.hash) {
+				var obj = Page.parse(src);
+				obj.hash = opts.hash;
+				src = Page.format(obj);
+			}
+			this.currentSrc = src;
 			this.iframe = this.firstElementChild;
-		}
-		this.classList.add('loading');
-		this.iframe.setAttribute('src', src);
+			if (state.scope.$consent == "no") {
+				this.classList.add('denied');
+				if (this.iframe) this.iframe.remove();
+				return;
+			}
+			if (!this.iframe) {
+				this.innerHTML = `<iframe width="100%" height="100%" frameborder="0" scrolling="no" allow="autoplay; fullscreen; accelerometer; gyroscope"></iframe>`;
+				this.iframe = this.firstElementChild;
+				if (!this.iframe.allow) this.iframe.allowFullscreen = true;
+			}
+			this.classList.add('loading');
+			this.iframe.setAttribute('src', src);
+		});
 	}
 	captureLoad() {
 		this.classList.remove('loading');
