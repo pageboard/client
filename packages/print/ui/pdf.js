764bb1a888f45f1b8a9b319ad6e4cb165e18f7ab
Jérémy Lal <kapouer@melix.org>
2020-01-06 16:56:39 +0100

    print: just disable IntersectionObserver so the stub kicks in

--- packages/print/ui/pdf.js
+++ packages/print/ui/pdf.js
@@ -1,7 +1,3 @@
-Page.setup(function(state) {
-	state.finish(function() {
-		Array.from(document.querySelectorAll('element-image')).forEach(function(node) {
-			node.reveal(true);
-		});
-	});
-});
+// HTMLCustomElement replaces it by a stub that calls back immediately
+window.IntersectionObserver = null;
+
