95c4ac19efe0b55443e617982b239344ed610138
Jérémy Lal <kapouer@melix.org>
2020-01-15 11:48:11 +0100

    image: use svg viewBox instead of width/height

--- packages/html/ui/image.js
+++ packages/html/ui/image.js
@@ -104,7 +104,7 @@ class HTMLElementImage extends HTMLCustomElement {
 		var w = img.getAttribute('width');
 		var h = img.getAttribute('height');
 		if (w && h) {
-			img.src = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(`<svg xmlns="http://www.w3.org/2000/svg" width="${w}" height="${h}"></svg>`);
+			img.src = "data:image/svg+xml," + encodeURIComponent(`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 ${w} ${h}"></svg>`);
 		}
 	}
 }
