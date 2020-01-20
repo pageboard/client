d58eb3c513e3f16d73288e3eb47a8e4f817389b0
Jérémy Lal <kapouer@melix.org>
2020-01-20 02:43:43 +0100

    email: port image code from html

--- packages/email/elements/image.js
+++ packages/email/elements/image.js
@@ -71,46 +71,13 @@ exports.mail_image = {
 		}
 	},
 	group: "mail_block",
-	buildLoc: function(url, d, meta) {
-		if (!url) return {
-			pathname: "",
-			query: {}
-		};
-		var loc = Page.parse(url);
-		if (loc.hostname && loc.hostname != document.location.hostname) {
-			loc = {
-				pathname: "/.api/image",
-				query: {
-					url: d.url
-				}
-			};
-		}
-		var r = d.crop || {};
-		if (r.x != null && r.y != null && r.width != null && r.height != null &&
-			(r.x != 50 || r.y != 50 || r.width != 100 || r.height != 100)) {
-			if (r.x - r.width / 2 < 0 || r.x + r.width / 2 > 100) {
-				r.width = 2 * Math.min(r.x, 100 - r.x);
-			}
-			if (r.y - r.height / 2 < 0 || r.y + r.height / 2 > 100) {
-				r.height = 2 * Math.min(r.y, 100 - r.y);
-			}
-			loc.query.ex = `x-${r.x}_y-${r.y}_w-${r.width}_h-${r.height}`;
-		}
-		if (r.zoom != null && r.zoom != 100) {
-			loc.query.rs = `z-${r.zoom}`;
-		}
-		if (meta && meta.mime == "image/svg+xml") loc.query.format = 'png';
-		return loc;
-	},
-	html: '<img src="[src]" alt="" />',
-	fuse: function(node, d, scope) {
-		var loc = this.buildLoc(d.url || this.resources.empty, d, (scope.$hrefs || {})[d.url]);
-		node.fuse(Object.assign({
-			src: Page.format(loc)
-		}, d), scope);
-	},
+	html: `<img is="element-mail-img"
+		data-src="[url|or:[$element.resources.empty]]"
+		data-crop="[crop.x];[crop.y];[crop.width];[crop.height];[crop.zoom]"
+		alt="[alt]" />`,
 	resources: {
 		empty: '../ui/empty.png'
-	}
+	},
+	scripts: ['../ui/image.js']
 };
 
---
+++ packages/email/ui/image.js
@@ -0,0 +1,81 @@
+class HTMLElementMailImage extends HTMLCustomElement {
+	static get defaults() {
+		return {
+			src: null,
+			crop: null
+		};
+	}
+	get crop() {
+		var [x, y, w, h, z] = (this.dataset.crop || ";;;;").split(";").map((x) => parseFloat(x));
+		if (isNaN(x)) x = 50;
+		if (isNaN(y)) y = 50;
+		if (isNaN(w)) w = 100;
+		if (isNaN(h)) h = 100;
+		if (isNaN(z)) z = 100;
+		return {x, y, w, h, z};
+	}
+	set crop({x, y, w, h, z}) {
+		this.dataset.crop = [x, y, w, h, z].join(';');
+	}
+	get image() {
+		return this;
+	}
+	patch(state) {
+		var loc = Page.parse(this.options.src);
+		var meta = state.scope.$hrefs && state.scope.$hrefs[loc.pathname] || {};
+		if (!meta || !meta.width || !meta.height) return;
+		if (meta.mime == "image/svg+xml") loc.query.format = 'png';
+
+		var img = this.image;
+		var w = meta.width;
+		var h = meta.height;
+
+		if (loc.hostname && loc.hostname != document.location.hostname) {
+			loc = {
+				pathname: "/.api/image",
+				query: {
+					url: this.options.src
+				}
+			};
+		}
+		var r = this.crop;
+		if (r.x != 50 || r.y != 50 || r.w != 100 || r.h != 100) {
+			if (Math.round((r.x - r.w / 2)*100) < 0 || Math.round((r.x + r.w / 2)*100) > 10000) {
+				r.w = 2 * Math.min(r.x, 100 - r.x);
+			}
+			if (Math.round((r.y - r.h / 2)*100) < 0 || Math.round((r.y + r.h / 2)*100) > 10000) {
+				r.h = 2 * Math.min(r.y, 100 - r.y);
+			}
+			loc.query.ex = `x-${r.x}_y-${r.y}_w-${r.w}_h-${r.h}`;
+		}
+		w = w * r.w / 100;
+		h = h * r.h / 100;
+		if (r.z != 100) {
+			loc.query.rs = `z-${r.z}`;
+		} else if (!isNaN(w) && !isNaN(h)) {
+			var zoom;
+			var rect = this.getBoundingClientRect();
+			var rw = rect.width;
+			var rh = rect.height;
+			if (rw == 0 && rh == 0) {
+				// don't show
+				return;
+			}
+			if (rw || rh) {
+				if (!rw) rw = rh * w / h;
+				if (!rh) rh = rw * h / w;
+				zoom = Math.ceil(Math.max(rw / w, rh / h) * 100 * (window.devicePixelRatio || 1));
+				// svg need to be resized to scale to its intrinsic dimension
+				if (zoom > 100) zoom = 100;
+			}
+			if (zoom) {
+				var zstep = 5;
+				zoom = Math.ceil(zoom / zstep) * zstep;
+				loc.query.rs = "z-" + zoom;
+			}
+		}
+		img.setAttribute('src', Page.format(loc));
+	}
+}
+
+HTMLCustomElement.define(`element-mail-img`, HTMLElementMailImage, 'img');
