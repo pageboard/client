bc5bf258eedbbd83c14ef9cda8ae78e2bcff1304
Jérémy Lal <kapouer@melix.org>
2020-01-21 13:59:49 +0100

    mail: fix groups

--- packages/email/elements/grid.js
+++ packages/email/elements/grid.js
@@ -22,25 +22,21 @@ exports.mail_row = {
 
 exports.mail_column_first = {
 	title: 'First Column',
-	group: "mail_block",
 	contents: "mail_block+",
 	html: '<th class="small-12 large-6 columns first"></th>'
 };
 
 exports.mail_column_last = {
 	title: 'Last Column',
-	group: "mail_block",
 	contents: "mail_block+",
 	html: '<th class="small-12 large-6 columns last"></th>'
 };
 
 exports.mail_column_expander = {
-	group: "mail_block",
 	html: '<th class="expander"></th>'
 };
 
 exports.mail_column = {
-	group: "mail_block",
 	title: "Column",
 	icon: '<b class="icon">Cl</b>',
 	contents: "mail_block+",
