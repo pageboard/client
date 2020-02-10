64db79d087d6d32e0d55fc3c40f72ad352884a9e
Jérémy Lal <kapouer@melix.org>
2020-02-10 11:57:17 +0100

    consent: run check on finish

--- packages/html/ui/consent.js
+++ packages/html/ui/consent.js
@@ -1,11 +1,13 @@
 Page.setup(function(state) {
-	var consent = Page.storage.get('consent');
-	if (consent === null && !Page.consent) {
-		consent = "yes";
-	}
-	state.scope.$consent = consent;
-	if (consent !== null) {
-		state.runChain('consent');
-	}
+	state.finish(() => {
+		var consent = Page.storage.get('consent');
+		if (consent === null && !Page.consent) {
+			consent = "yes";
+		}
+		state.scope.$consent = consent;
+		if (consent !== null) {
+			state.runChain('consent');
+		}
+	});
 });
 
