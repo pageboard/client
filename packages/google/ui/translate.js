7ad4653fd03deaf8b8e689a944bb468867e4c0f4
Jérémy Lal <kapouer@melix.org>
2020-02-11 11:16:57 +0100

    google translate: support dispose / reinstantiation correctly

--- packages/google/ui/translate.js
+++ packages/google/ui/translate.js
@@ -1,27 +1,26 @@
 class HTMLElementGoogleTranslate extends HTMLCustomElement {
 	setup(state) {
-		// delegate to singleton
-		this.constructor.setup(state);
-	}
-	static setup(state) {
 		if (this.style) {
 			Object.assign(document.body.style, this.style);
 		}
-		var me = this;
-		if (this.observer) return;
-		this.observer = new MutationObserver(function(list) {
-			list.forEach(function(mut) {
+		if (this.observer) this.close(state);
+		this.observer = new MutationObserver((list) => {
+			list.forEach((mut) => {
 				if (mut.attributeName == "style") {
 					var s = mut.target.style;
 					var top = parseInt(s.top);
-					me.shown = !isNaN(top) && top > 10;
-					if (me.shown) me.style = {
-						minHeight: s.minHeight,
-						top: s.top,
-						position: s.position
-					};
-					else if (top === 0) delete me.style;
-					me.setClass();
+					this.shown = !isNaN(top) && top > 10;
+					if (this.shown) {
+						this.style = {
+							minHeight: s.minHeight,
+							top: s.top,
+							position: s.position
+						};
+						this.started();
+					} else if (top === 0) {
+						delete this.style;
+					}
+					this.setClass();
 				}
 			});
 		});
@@ -31,80 +30,53 @@ class HTMLElementGoogleTranslate extends HTMLCustomElement {
 			this.show(state);
 		}
 	}
-	static show(state) {
+	show(state) {
 		if (document.body.isContentEditable) return;
-		if (!this.id) {
-			this.id = `id${Date.now()}`;
-			var cb = `HTMLElementGoogleTranslate_${this.id}`;
-			window[cb] = this.cb.bind(this, state);
-			var script = document.createElement('script');
-			script.src = `https://translate.google.com/translate_a/element.js?cb=${cb}`;
-			this.script = script;
-			document.head.appendChild(script);
-			return true;
-		} else if (this.inst) {
-			if (this.shown) return false;
-			this.inst.showBanner();
-			return true;
-		}
-		return false;
+		if (window.google && window.google.translate) delete window.google.translate;
+		this.id = `id${Date.now()}`;
+		var cb = `HTMLElementGoogleTranslate_${this.id}`;
+		window[cb] = this.cb.bind(this, state);
+		var script = document.createElement('script');
+		script.src = `https://translate.google.com/translate_a/element.js?cb=${cb}`;
+		this.script = script;
+		document.head.appendChild(script);
 	}
-	static setClass() {
+	setClass() {
 		document.documentElement.classList.toggle('google-translate-shown', !!this.shown);
 	}
-	static cb(state) {
+	cb(state) {
+		if (this.shown) return;
+		this.script.remove();
 		var TE = window.google.translate.TranslateElement;
-		this.inst = new TE({
+		if (!this.inst) this.inst = new TE({
 			pageLanguage: document.documentElement.lang,
 			layout: TE.InlineLayout.SIMPLE,
 			autoDisplay: true
 		});
 		this.inst.showBanner();
-
-		if (this.links) return;
-
-		var node = this.script;
-		var links = [];
-		while ((node = node.nextElementSibling)) {
-			if (node.href && /(https?:)?\/\/translate\.google.*\.com\//.test(node.href)) {
-				links.push(node);
-			}
+	}
+	started() {
+		if (this.translate) {
+			this.translate = false; // once
+			var btn = document.body.firstElementChild.firstElementChild.contentDocument.body.querySelector('[id=":0.confirm"]');
+			if (btn) btn.dispatchEvent(new MouseEvent("click"));
 		}
-		this.links = links;
-		setTimeout(function() {
-			var teSel = 'body > .skiptranslate, body > .goog-te-spinner-pos';
-			Array.from(document.querySelectorAll(teSel)).forEach(function(node) {
-				node.dataset.transitionKeep = true;
-			});
-		}, 100);
 	}
 	close() {
-		this.constructor.close(); // delegate to singleton
-	}
-	static close() {
-
+		if (this.observer) this.observer.disconnect();
+		delete this.observer;
+		if (this.inst) {
+			this.inst.dispose();
+			delete this.inst;
+		}
 	}
 	handleClick(e, state) {
 		e.stopPropagation();
 		e.preventDefault();
-		if (this.constructor.show(state)) {
-			window.scrollTo({top: 0});
-		}
+		this.show(state);
+		window.scrollTo({top: 0});
 	}
 }
-
-Page.init(function(state) {
-	var mergeHead = state.mergeHead;
-	var me = HTMLElementGoogleTranslate;
-	state.mergeHead = function(head) {
-		me.setClass();
-		if (me.links) me.links.forEach(function(node) {
-			head.appendChild(node.cloneNode(true));
-		});
-		return mergeHead.call(state, head);
-	};
-	state.mergeHead.HTMLElementGoogleTranslate = true;
-});
 
 Page.setup(function() {
 	HTMLCustomElement.define('element-google-translate', HTMLElementGoogleTranslate);
