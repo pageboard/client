173c375658d1ed0f87da00888af6de1027e1c3ef
Jérémy Lal <kapouer@melix.org>
2020-02-14 00:33:30 +0100

    html: audio, video builtins are similar, refactor

--- packages/html/elements/media.js
+++ packages/html/elements/media.js
@@ -1,5 +1,6 @@
 exports.video = {
 	title: "Vidéo",
+	priority: 1,
 	menu: 'media',
 	icon: '<i class="icon video"></i>',
 	group: "block",
@@ -67,10 +68,64 @@ exports.video = {
 		preload="metadata" autoplay="[autoplay]" loop="[loop]"
 		muted="[muted]" controls="[controls]"></video>`,
 	scripts: [
-		'../ui/video.js'
+		'../ui/media.js'
 	],
 	stylesheets: [
-		'../ui/video.css'
+		'../ui/media.css'
+	],
+	csp: {
+		media: ["'self'"]
+	}
+};
+
+exports.audio = {
+	title: "Audio",
+	priority: 1,
+	menu: 'media',
+	icon: '<i class="icon music"></i>',
+	group: "block",
+	properties: {
+		url: {
+			title: 'Address',
+			description: 'Local or remote URL',
+			nullable: true,
+			anyOf: [{
+				type: "string",
+				format: "uri"
+			}, {
+				type: "string",
+				format: "pathname"
+			}],
+			$helper: {
+				name: 'href',
+				filter: {
+					type: ["audio"]
+				}
+			}
+		},
+		autoplay: {
+			title: 'Autoplay',
+			type: 'boolean',
+			default: false
+		},
+		controls: {
+			title: 'Show controls',
+			type: 'boolean',
+			default: false
+		},
+		loop: {
+			title: 'Loop',
+			type: 'boolean',
+			default: false
+		}
+	},
+	html: `<audio is="element-audio" data-src="[url]"
+		preload="metadata" autoplay="[autoplay]" loop="[loop]" controls="[controls]"></audio>`,
+	scripts: [
+		'../ui/media.js'
+	],
+	stylesheets: [
+		'../ui/media.css'
 	],
 	csp: {
 		media: ["'self'"]
--- packages/html/ui/media.js
+++ packages/html/ui/media.js
@@ -1,10 +1,8 @@
-class HTMLElementVideo extends HTMLVideoElement {
-	static get defaults() {
-		return {
-			dataSrc: null
-		};
-	}
-
+const MixinMedia = {
+	init() {
+		this.promise = Promise.resolve();
+		this.promise.done = function() {};
+	},
 	patch(state) {
 		this.classList.remove('error', 'loading');
 		var loc = Page.parse(this.options.src);
@@ -12,7 +10,7 @@ class HTMLElementVideo extends HTMLVideoElement {
 		if (!meta || !meta.width || !meta.height) return;
 		this.width = meta.width;
 		this.height = meta.height;
-	}
+	},
 	reveal(state) {
 		var curSrc = this.options.src;
 		if (curSrc != this.currentSrc) {
@@ -24,18 +22,39 @@ class HTMLElementVideo extends HTMLVideoElement {
 			this.setAttribute('src', curSrc);
 		}
 		if (this.isContentEditable) this.pause();
-	}
+		return this.promise;
+	},
 	handleClick(e) {
 		if (this.isContentEditable) e.preventDefault();
-	}
+	},
 	captureLoad() {
+		this.promise.done();
 		this.classList.remove('loading');
-	}
+	},
 	captureError() {
+		this.promise.done();
 		this.classList.remove('loading');
 		this.classList.add('error');
 	}
+};
+
+class HTMLElementVideo extends HTMLVideoElement {
+	static get defaults() {
+		return {
+			dataSrc: null
+		};
+	}
 }
+Object.assign(HTMLElementVideo.prototype, MixinMedia);
+
+class HTMLElementAudio extends HTMLAudioElement {
+	static get defaults() {
+		return {
+			dataSrc: null
+		};
+	}
+}
+Object.assign(HTMLElementAudio.prototype, MixinMedia);
 
 HTMLCustomElement.define('element-video', HTMLElementVideo, 'video');
-
+HTMLCustomElement.define('element-audio', HTMLElementAudio, 'audio');
