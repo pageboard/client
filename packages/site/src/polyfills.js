85a3c36c05ad2d26c73d42bb532336da0a3a655f
Jérémy Lal <kapouer@melix.org>
2020-03-05 17:04:42 +0100

    site: more array polyfills

--- packages/site/src/polyfills.js
+++ packages/site/src/polyfills.js
@@ -1,9 +1,6 @@
 const Arr = Array.prototype;
-if (!NodeList.prototype.indexOf) NodeList.prototype.indexOf = Arr.indexOf;
-if (!HTMLCollection.prototype.indexOf) HTMLCollection.prototype.indexOf = Arr.indexOf;
+['filter', 'some', 'map', 'forEach', 'indexOf', 'find', 'includes'].forEach((name) => {
+	if (!NodeList.prototype[name]) NodeList.prototype[name] = Arr[name];
+	if (!HTMLCollection.prototype[name]) HTMLCollection.prototype[name] = Arr[name];
+});
 
-if (!NodeList.prototype.forEach) NodeList.prototype.forEach = Arr.forEach;
-if (!HTMLCollection.prototype.forEach) HTMLCollection.prototype.forEach = Arr.forEach;
-
-if (!NodeList.prototype.map) NodeList.prototype.map = Arr.map;
-if (!HTMLCollection.prototype.map) HTMLCollection.prototype.map = Arr.map;
