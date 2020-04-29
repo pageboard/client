75038822473c4be03face641dbe8d6889b2421e4
Jérémy Lal <kapouer@melix.org>
2020-04-29 13:55:15 +0200

    feed: nicer share button ui code

--- packages/feed/ui/share.js
+++ packages/feed/ui/share.js
@@ -5,17 +5,49 @@ class HTMLShareAnchorElement extends HTMLAnchorElement {
 		};
 	}
 	patch(state) {
-		const url = {
-			linkedin: 'https://www.linkedin.com/shareArticle?mini=true&url=[url]',
-			twitter: 'https://twitter.com/intent/tweet?text=[url]',
-			pinterest: 'https://pinterest.com/pin/create/button/?url=[url]',
-			facebook: 'https://www.facebook.com/sharer/sharer.php?u=[url]'
+		const href = document.location.origin + Page.format({
+			pathname: state.pathname,
+			query: Object.assign({}, state.query, {develop:undefined})
+		});
+		const card = {
+			url: href
+		};
+		['title', 'description', 'image'].forEach(name => {
+			const node = document.head.querySelector(`meta[property="og:${name}"]`);
+			if (node && node.content) card[name] = node.content;
+		});
+
+		const obj = {
+			linkedin: {
+				url: 'https://www.linkedin.com/shareArticle',
+				query: {
+					mini: true,
+					url: card.url
+				}
+			},
+			twitter: {
+				url: 'https://twitter.com/intent/tweet',
+				query: {
+					text: card.url
+				}
+			},
+			pinterest: {
+				url: 'https://pinterest.com/pin/create/button/',
+				query: {
+					url: card.url,
+					media: card.image,
+					description: card.title
+				}
+			},
+			facebook: {
+				url: 'https://www.facebook.com/sharer/sharer.php',
+				query: {
+					u: card.url
+				}
+			}
 		}[this.options.network];
-		if (url) {
-			this.href = url.fuse({
-				title: document.title,
-				url: encodeURIComponent(document.location.href),
-			});
+		if (obj) {
+			this.href = Page.format(Object.assign(Page.parse(obj.url), {query: obj.query}));
 		} else {
 			this.removeAttribute('href');
 		}
