c6ed654417989747592f9a6cd9de2e6b0517c708
Jérémy Lal <kapouer@melix.org>
2020-04-29 12:34:06 +0200

    feed: deprecate feed

--- packages/feed/elements/feed.js
+++ packages/feed/elements/feed.js
@@ -3,7 +3,6 @@ exports.feed = {
 	menu: "widget",
 	standalone: true,
 	group: "block",
-	icon: '<i class="newspaper outline icon"></i>',
 	properties: {
 		topics: {
 			title: 'Topics',
@@ -57,9 +56,6 @@ exports.feed = {
 		<div class="row">
 			<div class="column" block-content="extra"><p>Extra content</p></div>
 		</div>
-	</article>`,
-	scripts: [
-		'../ui/feed.js'
-	]
+	</article>`
 };
 
