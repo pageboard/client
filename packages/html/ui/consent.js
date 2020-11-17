291c2806ceabed5f243bcec4c60a7d4798795665
Jérémy Lal <kapouer@melix.org>
2020-11-17 14:42:04 +0100

    Simplify consent a lot

--- packages/html/ui/consent.js
+++ packages/html/ui/consent.js
@@ -2,29 +2,19 @@ Page.ready(function(state) {
 	HTMLCustomElement.define(`element-consent`, HTMLCustomConsentElement, 'form');
 });
 
-Page.State.prototype.consent = function(fn) {
-	// listen for consent decisions
-	this.chain('consent', () => {
-		if (this.scope.$consent != null) fn(this.scope.$consent == "yes");
-	});
-	var consent = this.scope.$consent;
+Page.State.prototype.consent = function (fn) {
+	var consent = this.scope.$consent = Page.storage.get('consent');
+	this.chain('consent', fn);
 	if (consent === undefined) {
-		if (this.stage == "patch") {
-			fn(true);
-		} else {
-			// setup not finished yet
-			HTMLCustomConsentElement.waiting = true;
-		}
+		HTMLCustomConsentElement.waiting = true;
 	} else if (consent === null) {
 		// setup finished but no consent is done yet, ask consent
 		this.reconsent();
-	} else {
-		// setup finished and consent was decided
-		fn(consent == "yes");
 	}
 };
 
-Page.State.prototype.reconsent = function(fn) {
+Page.State.prototype.reconsent = function (fn) {
+	if (fn) this.consent(fn);
 	var consent = this.scope.$consent;
 	var asking = false;
 	if (consent != "yes") {
@@ -32,19 +22,12 @@ Page.State.prototype.reconsent = function(fn) {
 	}
 	if (!asking) {
 		if (consent == null) this.scope.$consent = "yes";
-		if (!this.chains.consent) this.runChain('consent');
-		else if (fn) fn(this.scope.$consent == "yes");
-	} else {
-		if (fn) this.chain('consent', () => {
-			if (this.scope.$consent != null) fn(this.scope.$consent == "yes");
-		});
 	}
 	return asking;
 };
 
 Page.setup(function(state) {
 	state.finish(function() {
-		state.scope.$consent = Page.storage.get('consent');
 		var run = true;
 		if (HTMLCustomConsentElement.waiting) {
 			if (state.reconsent()) run = false;
@@ -73,12 +56,13 @@ class HTMLCustomConsentElement extends HTMLFormElement {
 		if (tmpl.content && tmpl.children.length == 0) {
 			tmpl.appendChild(tmpl.content);
 		}
-		state.chain('consent', (state) => {
-			window.HTMLCustomFormElement.prototype.fill.call(this, {
-				consent: state.scope.$consent
-			});
-			if (this.options.transient) this.classList.remove('visible');
+		state.consent(this);
+	}
+	handleConsent(state) {
+		window.HTMLCustomFormElement.prototype.fill.call(this, {
+			consent: state.scope.$consent
 		});
+		if (this.options.transient) this.classList.remove('visible');
 	}
 	handleSubmit(e, state) {
 		if (e.type == "submit") e.preventDefault();
--- packages/html/ui/embed.js
+++ packages/html/ui/embed.js
@@ -17,38 +17,40 @@ class HTMLElementEmbed extends HTMLCustomElement {
 		this.promise.done = done;
 		this.classList.add('waiting');
 
-		state.consent((agreed) => {
-			this.classList.remove('waiting', 'denied');
-			var opts = this.options;
-			var src = opts.src;
-			if (opts.hash) {
-				var obj = Page.parse(src);
-				obj.hash = opts.hash;
-				src = Page.format(obj);
-			}
-			this.iframe = this.firstElementChild;
-			if (!agreed) {
-				this.classList.add('denied');
-				if (this.iframe) this.iframe.remove();
-				done();
-				return;
-			}
-			if (src != this.currentSrc) {
-				this.classList.remove('error');
-				this.currentSrc = src;
-				if (!this.iframe) {
-					this.innerHTML = `<iframe width="100%" height="100%" frameborder="0" scrolling="no" allow="autoplay; fullscreen; accelerometer; gyroscope"></iframe>`;
-					this.iframe = this.firstElementChild;
-					if (!this.iframe.allow) this.iframe.allowFullscreen = true;
-				}
-				this.classList.add('loading');
-
-				this.iframe.setAttribute('src', src);
-			} else {
-				done();
-			}
-		});
+		state.consent(this);
 		return this.promise;
+	}
+	consent(state) {
+		var agreed = state.scope.$consent == "yes";
+		this.classList.remove('waiting', 'denied');
+		var opts = this.options;
+		var src = opts.src;
+		if (opts.hash) {
+			var obj = Page.parse(src);
+			obj.hash = opts.hash;
+			src = Page.format(obj);
+		}
+		this.iframe = this.firstElementChild;
+		if (!agreed) {
+			this.classList.add('denied');
+			if (this.iframe) this.iframe.remove();
+			this.promise.done();
+			return;
+		}
+		if (src != this.currentSrc) {
+			this.classList.remove('error');
+			this.currentSrc = src;
+			if (!this.iframe) {
+				this.innerHTML = `<iframe width="100%" height="100%" frameborder="0" scrolling="no" allow="autoplay; fullscreen; accelerometer; gyroscope"></iframe>`;
+				this.iframe = this.firstElementChild;
+				if (!this.iframe.allow) this.iframe.allowFullscreen = true;
+			}
+			this.classList.add('loading');
+
+			this.iframe.setAttribute('src', src);
+		} else {
+			this.promise.done();
+		}
 	}
 	captureClick(e, state) {
 		if (this.matches('.denied')) state.reconsent();
