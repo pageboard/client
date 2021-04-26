67e841e339969d0a584a33b2005da319edecc0c9
Jérémy Lal <kapouer@melix.org>
2021-04-26 23:53:32 +0200

    embed: fix navigation and hash changes

--- packages/html/ui/embed.js
+++ packages/html/ui/embed.js
@@ -24,31 +24,37 @@ class HTMLElementEmbed extends VirtualHTMLElement {
 		var consent = state.scope.$consent;
 		this.classList.toggle('denied', consent == "no");
 		this.classList.toggle('waiting', consent == null);
-		var opts = this.options;
-		var src = opts.src;
-		if (opts.hash) {
-			var obj = Page.parse(src);
-			obj.hash = opts.hash;
-			src = Page.format(obj);
-		}
-		this.iframe = this.firstElementChild;
+
+		this.iframe = this.querySelector('iframe');
 		if (consent != "yes") {
 			if (this.iframe) this.iframe.remove();
 			this.promise.done();
 			return;
 		}
-		if (src != this.currentSrc) {
-			this.classList.remove('error');
-			this.currentSrc = src;
-			if (!this.iframe) {
-				this.innerHTML = `<iframe width="100%" height="100%" frameborder="0" scrolling="no" allow="autoplay; fullscreen; accelerometer; gyroscope"></iframe>`;
-				this.iframe = this.firstElementChild;
-				if (!this.iframe.allow) this.iframe.allowFullscreen = true;
-			}
-			this.classList.add('loading');
+		if (!this.iframe) {
+			this.innerHTML = `<iframe width="100%" height="100%" frameborder="0" scrolling="no" allow="autoplay; fullscreen; accelerometer; gyroscope"></iframe>`;
+			this.iframe = this.firstElementChild;
+		}
+		if (!this.iframe.allow) this.iframe.allowFullscreen = true;
 
-			this.iframe.setAttribute('src', src);
+		var opts = this.options;
+		var prev = Page.parse(this.currentSrc || this.iframe.src || "about:blank");
+		var cur = Page.parse(opts.src || "about:blank");
+		cur.hash = opts.hash;
+		this.currentSrc = Page.format(cur);
+
+		if (Page.samePath(cur, prev) == false) {
+			this.classList.remove('error');
+			this.classList.add('loading');
+			this.iframe.setAttribute('src', this.currentSrc);
 		} else {
+			if (cur.hash != prev.hash) {
+				try {
+					this.iframe.contentWindow.location.replace(this.currentSrc);
+				} catch (err) {
+					this.iframe.setAttribute('src', this.currentSrc);
+				}
+			}
 			this.promise.done();
 		}
 	}
