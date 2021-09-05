3510dc74ce57b815cd276652b500f6c36702e0b7
Jérémy Lal <kapouer@melix.org>
2021-09-06 00:38:02 +0200

    html: simple pagination with infinite loading

--- packages/html/elements/template.js
+++ packages/html/elements/template.js
@@ -44,3 +44,39 @@ exports.query_tags = {
 	]
 };
 
+exports.pagination = {
+	priority: 10, // after fetch
+	title: "Pagination",
+	icon: '<b class="icon">±N</b>',
+	menu: "link",
+	context: 'menu//',
+	group: "menu_item",
+	properties: {
+		name: {
+			title: 'Offset name',
+			description: 'Query parameter used by fetch block',
+			type: 'string',
+			format: 'id',
+			default: 'offset'
+		},
+		value: {
+			title: 'Offset value',
+			description: 'Integer, can be negative',
+			type: 'integer',
+			default: 10
+		},
+		infinite: {
+			title: 'Infinite loading',
+			type: 'boolean',
+			default: false
+		}
+	},
+	contents: "inline*",
+	html: `<a class="item" is="element-pagination" data-name="[name]" data-value="[value]" data-infinite="[infinite]"></a>`,
+	stylesheets: [
+		'../ui/pagination.css'
+	],
+	scripts: [
+		'../ui/pagination.js'
+	]
+};
---
+++ packages/html/ui/pagination.js
@@ -0,0 +1,118 @@
+class HTMLElementPagination extends HTMLAnchorElement {
+	#observer
+	#queue
+	#reached
+	#size
+	#continue = false
+
+	constructor() {
+		super();
+		this.init?.();
+	}
+
+	patch(state) {
+		if (this.isContentEditable || this.closest('[block-content="template"]')) {
+			return;
+		}
+		const name = this.dataset.name || 'offset';
+		const off = parseInt(state.query[name]) || 0;
+		const delta = parseInt(this.dataset.value) || 0;
+		const cur = off + delta;
+		const disabled = cur < 0 || !this.#findFetch() || this.#continue && delta < 0;
+		this.classList.toggle('disabled', disabled);
+
+		if (disabled) {
+			this.removeAttribute('href');
+		} else {
+			this.setAttribute('href', Page.format({
+				pathname: state.pathname,
+				query: Object.assign({}, state.query, {
+					[name]: cur || undefined
+				})
+			}));
+		}
+		state.finish(() => {
+			this.#reached = false;
+			if (this.#updateFetchSize() == false) {
+				this.removeAttribute('href');
+				this.classList.toggle('disabled', true);
+			}
+			if (off == 0) {
+				this.#continue = true;
+			}
+		});
+	}
+
+	#more(state) {
+		this.#queue = this.#queue.then(() => {
+			if (!this.#continue || !this.hasAttribute('href')) {
+				return;
+			}
+			const fetch = this.#findFetch();
+			if (fetch) {
+				fetch.infinite = true;
+				return state.push(this.getAttribute('href'));
+			}
+		}).then(() => {
+			this.#reached = false;
+		});
+	}
+
+	#updateFetchSize() {
+		const old = this.#size;
+		const cur = this.#size = this.#findFetch()?.ownView.children.length ?? 0;
+		if (this.#continue) return old != cur;
+		else return cur != 0;
+	}
+
+	#findFetch() {
+		const name = this.dataset.name || 'offset';
+		const fetch = this.ownerDocument.querySelector(
+			`element-template[block-type="fetch"][parameters~="$query.${name}|or:0"]`
+		);
+		if (!fetch) {
+			console.warn(`Pagination need Fetch to use $query.${name} parameter`);
+		}
+		return fetch;
+	}
+
+	handleClick(e, state) {
+		const fetch = this.#findFetch();
+		if (fetch) fetch.infinite = false;
+	}
+
+	setup(state) {
+		const name = this.dataset.name || 'offset';
+		const off = parseInt(state.query[name]) || 0;
+		if (off != 0) this.#continue = false;
+		if (this.isContentEditable || !this.#infinite) return;
+		this.#queue = Promise.resolve();
+		this.#observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				if (this.#reached) return;
+				if (this.offsetParent && (entry.intersectionRatio || 0) !== 0) {
+					this.#reached = true;
+					this.#more(state);
+				}
+			});
+		}, {
+			threshold: [1]
+		});
+		this.#observer.observe(this);
+	}
+
+	close() {
+		this.#queue = null;
+		if (this.#observer) {
+			this.#observer.unobserve(this);
+			this.#observer.disconnect();
+			this.#observer = null;
+		}
+	}
+
+	get #infinite() {
+		return this.hasAttribute('data-infinite');
+	}
+}
+
+VirtualHTMLElement.define('element-pagination', HTMLElementPagination, "a");
