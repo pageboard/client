b429cf112d1cb8cc9e46556034c09c510f8c9d3d
Jérémy Lal <kapouer@melix.org>
2022-11-14 16:12:41 +0100

    blog is a bundle now

--- packages/feed/elements/blogs.js
+++ packages/feed/elements/blogs.js
@@ -2,7 +2,7 @@ exports.blog = {
 	title: 'Blog',
 	icon: '<i class="newspaper outline icon"></i>',
 	standalone: true,
-	group: 'block',
+	bundle: true,
 	properties: {
 		title: {
 			title: 'Title',
