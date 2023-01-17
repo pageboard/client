181c2d29bf1e61ddde590a3dbe2cb6c0fa86b7d9
Jérémy Lal <kapouer@melix.org>
2023-01-17 11:44:01 +0100

    html: put query-tags, pagination into their own file

---
+++ packages/html/elements/query-tags.js
@@ -0,0 +1,30 @@
+exports.query_tags = {
+	priority: 10, // after fetch
+	title: 'Tags',
+	icon: '<i class="tags icon"></i>',
+	menu: "form",
+	group: "block",
+	properties: {
+		form: {
+			title: 'Form name',
+			type: 'string',
+			format: 'id',
+			nullable: true
+		}
+	},
+	contents: {
+		id: 'title',
+		nodes: 'inline*'
+	},
+	html: `<element-query-tags for="[form]">
+		<div block-content="title">Filters:</div>
+		<div class="ui labels"></div>
+	</element-query-tags>`,
+	stylesheets: [
+		'../lib/components/label.css',
+		'../ui/query-tags.css'
+	],
+	scripts: [
+		'../ui/query-tags.js'
+	]
+};
--- packages/html/elements/template.js
+++ packages/html/elements/template.js
@@ -15,74 +15,7 @@ exports.message.properties.inverted = {
 	type: 'boolean',
 	default: false
 };
+
 exports.message.stylesheets.unshift(
 	'../lib/components/message.css'
 );
-
-exports.query_tags = {
-	priority: 10, // after fetch
-	title: 'Tags',
-	icon: '<i class="tags icon"></i>',
-	menu: "form",
-	group: "block",
-	properties: {
-		form: {
-			title: 'Form name',
-			type: 'string',
-			format: 'id',
-			nullable: true
-		}
-	},
-	contents: {
-		id: 'title',
-		nodes: 'inline*'
-	},
-	html: `<element-query-tags for="[form]">
-		<div block-content="title">Filters:</div>
-		<div class="ui labels"></div>
-	</element-query-tags>`,
-	stylesheets: [
-		'../lib/components/label.css',
-		'../ui/query-tags.css'
-	],
-	scripts: [
-		'../ui/query-tags.js'
-	]
-};
-
-exports.pagination = {
-	priority: 13, // after fetch and after menu items
-	title: "Pagination",
-	icon: '<b class="icon">±N</b>',
-	menu: "link",
-	context: 'menu//',
-	group: "menu_item",
-	properties: {
-		name: {
-			title: 'Offset name',
-			description: 'Query parameter used by fetch block',
-			type: 'string',
-			format: 'id',
-			default: 'offset'
-		},
-		value: {
-			title: 'Offset value',
-			description: 'Integer, can be negative',
-			type: 'integer',
-			default: 10
-		},
-		infinite: {
-			title: 'Infinite loading',
-			type: 'boolean',
-			default: false
-		}
-	},
-	contents: "inline*",
-	html: `<a class="item" is="element-pagination" data-name="[name]" data-value="[value]" data-infinite="[infinite]"></a>`,
-	stylesheets: [
-		'../ui/pagination.css'
-	],
-	scripts: [
-		'../ui/pagination.js'
-	]
-};
