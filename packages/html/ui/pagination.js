7b11b3d87736d95982c2d7e6a29171f4bc513046
Jérémy Lal <kapouer@melix.org>
2023-01-18 16:00:54 +0100

    pagination: use an helper after template's div.view

--- packages/html/ui/pagination.js
+++ packages/html/ui/pagination.js
@@ -2,7 +2,7 @@ class HTMLElementPagination extends HTMLAnchorElement {
 	#observer;
 	#queue;
 	#locked;
-	#visible;
+	#moreDown;
 	#name = 'offset';
 
 	constructor() {
@@ -13,36 +13,32 @@ class HTMLElementPagination extends HTMLAnchorElement {
 	patch(state) {
 		if (this.isContentEditable) return;
 		state.finish(() => {
-			const node = this.#getFetchNode();
-			if (!node) {
-				console.warn("pagination does not find fetch node", this.dataset.fetch);
-				return;
-			}
-			const name = this.#name;
-			const start = parseInt(node.dataset.start) || 0;
-			const stop = parseInt(node.dataset.stop) || 0;
-			const limit = parseInt(node.dataset.limit) || 10;
-			const count = parseInt(node.dataset.count) || 0;
-			const sign = this.dataset.dir == "-" ? -1 : +1;
-			const cur = sign > 0 ? stop : (start - limit);
-
-
-			this.setAttribute('href', Page.format({
-				pathname: state.pathname,
-				query: {
-					...state.query,
-					[name]: cur || undefined
-				}
-			}));
-			this.disabled = sign < 0 && cur + limit <= 0 || sign > 0 && count < limit;
-			this.#locked = false;
+			this.#update(state);
 		});
 	}
 
-	#getFetchNode() {
-		return this.ownerDocument.querySelector(`element-template[action="/.api/query/${this.dataset.fetch}"]`);
+	#update(state) {
+		const node = this.#node;
+		if (!node) {
+			console.warn("pagination does not find fetch node", this.dataset.fetch);
+			return;
+		}
+		const name = this.#name;
+		const start = parseInt(node.dataset.start) || 0;
+		const stop = parseInt(node.dataset.stop) || 0;
+		const limit = parseInt(node.dataset.limit) || 10;
+		const count = parseInt(node.dataset.count) || 0;
+		const sign = this.dataset.dir == "-" ? -1 : +1;
+		const cur = sign > 0 ? stop : (start - limit);
+		this.setAttribute('href', Page.format({
+			pathname: state.pathname,
+			query: {
+				...state.query,
+				[name]: cur || undefined
+			}
+		}));
+		this.disabled = sign < 0 && cur + limit <= 0 || sign > 0 && count < limit;
 	}
-
 	handleClick(e) {
 		if (this.disabled) {
 			e.preventDefault();
@@ -52,52 +48,67 @@ class HTMLElementPagination extends HTMLAnchorElement {
 
 	set disabled(val) {
 		this.classList.toggle('disabled', val);
+		if (val && this.parentNode.matches('.menu') && this.parentNode.children.length == 1) {
+			this.parentNode.hidden = true;
+		} else if (!val) {
+			this.parentNode.hidden = false;
+		}
 	}
 
 	get disabled() {
 		return this.classList.contains('disabled');
 	}
 
-	#more(state) {
+	get #node() {
+		return this.ownerDocument.querySelector(
+			`element-template[action="/.api/query/${this.dataset.fetch}"]`
+		);
+	}
+
+	#more(state, node) {
 		this.#locked = true;
+
 		if (this.#queue) this.#queue = this.#queue.then(() => {
-			if (!this.disabled) return state.push(this.getAttribute('href'));
+			if (this.disabled) return;
+			const { query } = state;
+			state.query = Object.assign({}, query, Page.parse(this.getAttribute('href')).query);
+			return node.fetch(state)
+				.then(() => this.#update(state))
+				.then(() => {
+					state.query = query;
+				});
 		}).then(() => {
 			this.#locked = false;
 		});
 	}
 
-	paint(state) {
-		const name = this.#name;
-		const off = parseInt(state.query[name]) || 0;
-		if (off == 0) {
-			if (this.#visible) this.#more(state);
-		}
-	}
-
 	setup(state) {
-		if (this.isContentEditable || !this.dataset.infinite) return;
+		if (this.isContentEditable) return;
+		if (!this.dataset.infinite || this.dataset.dir == "-") return;
 		this.#queue = Promise.resolve();
+		const node = this.#node;
 		this.#observer = new IntersectionObserver(entries => {
 			entries.forEach(entry => {
-				if (this.#locked) return;
-				if (this.offsetParent && (entry.intersectionRatio || 0) !== 0) {
-					this.#visible = true;
-					this.#more(state);
+				if (entry.isIntersecting) {
+					this.#moreDown = true;
+					if (!this.#locked) this.#more(state, node);
 				} else {
-					this.#visible = false;
+					this.#moreDown = false;
 				}
 			});
 		}, {
-			threshold: [1]
+			threshold: [1.0]
 		});
-		this.#observer.observe(this);
+		const observed = node.lastElementChild.matches('.pagination-helper') ?
+			node.lastElementChild :
+			node.appendChild(node.dom(`<div class="pagination-helper"></div>`));
+		this.#observer.observe(observed);
 	}
 
 	close() {
 		this.#queue = null;
 		if (this.#observer) {
-			this.#observer.unobserve(this);
+			this.#observer.unobserve(this.#node.lastElementChild);
 			this.#observer.disconnect();
 			this.#observer = null;
 		}
