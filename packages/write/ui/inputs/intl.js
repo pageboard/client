62ef7ef62cb0228fd8379aa31590e7cfc2fad12e
Jérémy Lal <kapouer@melix.org>
2023-02-14 11:36:47 +0100

    Comment about that

--- packages/write/ui/inputs/intl.js
+++ packages/write/ui/inputs/intl.js
@@ -1,5 +1,5 @@
 Pageboard.schemaFilters.intl = function IntlFilter(key, opts, schema) {
-	const list = Intl.supportedValuesOf(opts.of);
+	const list = Intl.supportedValuesOf(opts.of); // experimental Intl, not polyfilled
 	delete schema.type;
 	schema.anyOf = [];
 	for (const tz of list) {
