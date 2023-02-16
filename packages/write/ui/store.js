eb8bc5f44fd1aa9ce2ad9a610a6ca314f14011bf
Jérémy Lal <kapouer@melix.org>
2023-02-16 17:56:57 +0100

    store: need this in callback

--- packages/write/ui/store.js
+++ packages/write/ui/store.js
@@ -4,7 +4,7 @@ Pageboard.Controls.Store = class Store {
 	static generated = {};
 
 	constructor(editor, node) {
-		this.debounceUpdate = Page.debounce(this.realUpdate, 500);
+		this.debounceUpdate = Page.debounce(() => this.realUpdate(), 500);
 		this.node = node;
 		this.editor = editor;
 
