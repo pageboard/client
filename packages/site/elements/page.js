3bba2f78bfbd35bad003e1c425c7d9aacccdd91c
Jérémy Lal <kapouer@melix.org>
2023-04-25 10:13:09 +0200

    page: optional $links here

--- packages/site/elements/page.js
+++ packages/site/elements/page.js
@@ -89,7 +89,7 @@ exports.page = {
 		<meta http-equiv="Status" content="301 Moved Permanently[transition.from|prune:*:1]">
 		<meta http-equiv="Location" content="[redirect|neq:[url]|fail:*::1][$loc.search]">
 		<meta http-equiv="Status" content="301 Matching Location">
-		<meta http-equiv="Location" content="[$links.found|fail:*::1]">
+		<meta http-equiv="Location" content="[$links?.found|fail:*::1]">
 		<meta http-equiv="Content-Security-Policy" content="[$elements|as:csp]">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta name="robots" content="[noindex|and:noindex|fail:*]">
