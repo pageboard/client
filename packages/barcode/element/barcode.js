2cad64b5845e1b34ccbc4cc7ffd4c8f5debcea26
Jérémy Lal <kapouer@melix.org>
2023-07-06 23:33:56 +0200

    Hey, we have barcodes !

---
+++ packages/barcode/element/barcode.js
@@ -0,0 +1,45 @@
+exports.barcode = {
+	priority: 21,
+	title: "Barcode",
+	icon: '<i class="barcode icon"></i>',
+	menu: "media",
+	bundle: true,
+	group: 'block',
+	properties: {
+		bcid: {
+			title: 'Barcode type',
+			anyOf: [{
+				const: 'qrcode',
+				title: 'QR code'
+			}, {
+				const: 'ean13',
+				title: 'EAN-13'
+			}, {
+				const: 'isbn',
+				title: 'ISBN'
+			}]
+		},
+		text: {
+			title: 'Text to encode',
+			type: 'string',
+			format: 'singleline'
+		},
+		width: {
+			title: 'Width',
+			type: 'integer',
+			default: 40
+		},
+		height: {
+			title: 'Height',
+			type: 'integer',
+			default: 20
+		}
+	},
+	html: `<element-barcode data-bcid="[bcid]" data-text="[text]" data-scale-x="[width]" data-scale-y="[height]"></element-barcode>`,
+	scripts: [
+		'../lib/barcode.js'
+	],
+	stylesheets: [
+		'../ui/barcode.css'
+	]
+};
---
+++ packages/barcode/src/barcode.js
@@ -0,0 +1,26 @@
+import bwipjs from 'bwip-js';
+
+class HTMLElementBarcode extends Page.Element {
+	static defaults = {
+		bcid: null,
+		text: null,
+		scaleX: x => parseInt(x) || 0,
+		scaleY: x => parseInt(x) || 0
+	};
+
+	async patch(state) {
+		const doc = this.ownerDocument;
+		const canvas = doc.createElement('canvas');
+		const img = this.querySelector('img') || this.appendChild(doc.createElement('img'));
+		img.classList.remove('error');
+		try {
+			bwipjs.toCanvas(Object.assign({ includetext: true }, this.options), canvas);
+			img.src = canvas.toDataURL('image/png');
+		} catch (e) {
+			console.error(e);
+			img.classList.add('error');
+		}
+	}
+}
+
+Page.define('element-barcode', HTMLElementBarcode);
