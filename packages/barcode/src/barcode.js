0936d186121b709de08c38edc3f80cf299031dde
Jérémy Lal <kapouer@melix.org>
2023-09-07 11:23:12 +0200

    barcode: correct dimensions and ratios

--- packages/barcode/src/barcode.js
+++ packages/barcode/src/barcode.js
@@ -14,20 +14,14 @@ class HTMLElementBarcode extends Page.Element {
 		const canvas = doc.createElement('canvas');
 		const img = this.querySelector('img') || this.appendChild(doc.createElement('img'));
 		img.classList.remove('error');
+		img.width = this.options.scaleX;
+		img.height = this.options.scaleY;
+		img.style.height = this.options.dimension;
 		try {
 			bwipjs.toCanvas(Object.assign({
 				includetext: true
 			}, this.options), canvas);
 			img.src = canvas.toDataURL('image/png');
-			const dim = parseFloat(this.options.dimension);
-			if (Number.isNaN(dim)) {
-				delete img.width;
-				delete img.height;
-			} else {
-				const unit = /([a-z]+)$/.exec(this.options.dimension)?.[1] ?? 'px';
-				img.width = this.options.scaleX * (dim < 1 ? 1 : dim) + unit;
-				img.height = this.options.scaleY * (dim < 1 ? 1 : dim) + unit;
-			}
 		} catch (e) {
 			img.classList.add('error');
 		}
