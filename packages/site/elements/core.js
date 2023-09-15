55266b38123592633601b6223bb8510f91adaa63
Jérémy Lal <kapouer@melix.org>
2023-09-15 08:58:37 +0200

    Remove content from core, moved to server

--- packages/site/elements/core.js
+++ packages/site/elements/core.js
@@ -19,27 +19,3 @@ exports.otp = {
 	html: `<img class="otpkey" src="[uri]" width="196" height="196" />`
 };
 
-exports.content = {
-	title: 'Content',
-	properties: {
-		name: {
-			title: 'Name',
-			type: 'string',
-			format: 'name'
-		},
-		lang: {
-			title: 'Language',
-			type: 'string',
-			format: 'lang'
-		},
-		text: {
-			title: 'Text',
-			type: 'string'
-		},
-		valid: {
-			title: 'Valid',
-			type: 'boolean',
-			default: false
-		}
-	}
-};
