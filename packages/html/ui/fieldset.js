3385f8a5295cf069f7d92fa4a01569b16d3b0659
Jérémy Lal <kapouer@melix.org>
2023-11-02 11:55:06 +0100

    fieldset: ui

--- packages/html/ui/fieldset.js
+++ packages/html/ui/fieldset.js
@@ -1,14 +1,21 @@
 class HTMLElementFieldSet extends Page.create(HTMLFieldSetElement) {
 	static defaults = {
 		dataName: null,
+		dataOp: null,
 		dataValue: null
 	};
+
+	#compare(to) {
+		const { value } = this.options;
+		if (this.options.op == "neq") return value != to;
+		else return value == to;
+	}
 
 	fill(query) {
 		if (this.isContentEditable || !this.options?.name || !this.form) return;
 		if (!query) query = this.form.read(true);
 		const val = query[this.options.name];
-		const disabled = this.disabled = this.hidden = val != this.options.value;
+		const disabled = this.disabled = this.hidden = !this.#compare(val);
 		for (const node of this.querySelectorAll('[name]')) {
 			node.disabled = disabled;
 		}
