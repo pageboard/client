e472f9f3ea9610f98cc4b119601309f96686a5ac
Jérémy Lal <kapouer@melix.org>
2023-11-06 16:21:42 +0100

    otp element is in server schemas

--- packages/site/elements/core.js
+++ packages/site/elements/core.js
@@ -3,18 +3,3 @@ exports.core = {
 	scripts: ["../lib/pageboard.js"],
 	priority: -1000 // way before page group
 };
-
-
-exports.otp = {
-	bundle: 'user',
-	group: 'block',
-	virtual: true,
-	properties: {
-		uri: {
-			type: 'string'
-		}
-	},
-	tag: 'img.otpkey',
-	html: `<img class="otpkey" src="[uri]" width="196" height="196" />`
-};
-
