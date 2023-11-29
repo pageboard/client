1de636eaa6b4272b94f686cc5337e0b08f347580
Jérémy Lal <kapouer@melix.org>
2023-11-29 19:13:04 +0100

    email: use inlines from html

--- packages/email/elements/inlines.js
+++ packages/email/elements/inlines.js
@@ -5,134 +5,42 @@ exports.mail_hard_break = {
 };
 
 exports.mail_strong = {
-	title: "Strong",
-	priority: 12,
-	icon: '<i class="icon bold"></i>',
-	contents: "text*",
-	inplace: true,
-	inline: true,
-	group: "mail_inline",
-	html: '<strong></strong>'
+	...exports.strong,
+	group: "mail_inline"
 };
 
 exports.mail_em = {
-	title: "Emphasize",
-	priority: 12,
-	icon: '<i class="icon italic"></i>',
-	contents: "text*",
-	inline: true,
-	inplace: true,
-	group: "mail_inline",
-	html: '<em></em>'
+	...exports.em,
+	group: "mail_inline"
 };
 
 exports.mail_light = {
-	title: "Light",
-	priority: 12,
-	icon: '<i class="icon" style="font-weight:lighter">L</i>',
-	contents: "text*",
-	inline: true,
-	inplace: true,
-	group: "mail_inline",
-	html: '<span class="lighter"></span>'
+	...exports.light,
+	group: "mail_inline"
 };
 
 exports.mail_sup = {
-	title: "Sup",
-	priority: 12,
-	icon: '<b class="icon">S<sup>up</sup></b>',
-	contents: "text*",
-	inline: true,
-	inplace: true,
-	group: "mail_inline",
-	html: '<sup></sup>'
+	...exports.sup,
+	group: "mail_inline"
+};
+
+exports.mail_style = {
+	...exports.style,
+	group: "mail_inline"
 };
 
 exports.mail_caps = {
-	title: "Capitalization",
-	priority: 12,
-	icon: '<span class="icon">Aa</span>',
-	contents: "text*",
-	inline: true,
-	inplace: true,
-	group: "mail_inline",
-	tag: 'span[block-type="caps"]',
-	properties: {
-		transform: {
-			title: 'Transform',
-			default: "uppercase",
-			anyOf: [{
-				const: "uppercase",
-				title: "Upper",
-				icon: '<span class="icon">A</span>'
-			}, {
-				const: "lowercase",
-				title: "Lower",
-				icon: '<span class="icon">a</span>'
-			}, {
-				const: "capitalize",
-				title: "All caps",
-				icon: '<span class="icon" style="text-transform:capitalize">Aa</span>'
-			}, {
-				const: "smallcaps",
-				title: "Small caps",
-				icon: '<span class="icon" style="font-variant:small-caps">Aa</span>'
-			}]
-		}
-	},
-	html: '<span class="[transform]"></span>'
+	...exports.caps,
+	group: "mail_inline"
 };
 
 exports.mail_color = {
-	title: "Color",
-	priority: 12,
-	icon: `<i class="icon" style="background-image:
-		linear-gradient(
-			to right,
-			red, orange, yellow, green, blue, purple
-		);"></i>`,
-	properties: {
-		color: {
-			default: "",
-			anyOf: [{
-				const: "white",
-				title: "White"
-			}, {
-				const: "",
-				title: "Black"
-			}, {
-				const: "red",
-				title: "Red"
-			}, {
-				const: "orange",
-				title: "Orange"
-			}, {
-				const: "yellow",
-				title: "Yellow"
-			}, {
-				const: "green",
-				title: "Green"
-			}, {
-				const: "blue",
-				title: "Blue"
-			}, {
-				const: "purple",
-				title: "Purple"
-			}]
-		}
-	},
-	parse: function(dom) {
-		const prop = this.properties.color;
-		let color = prop.anyOf.find(item => {
-			return item.const && dom.classList.contains(item.const);
-		});
-		if (color) color = color.const;
-		else color = prop.default;
-		return {color: color};
-	},
-	contents: "text*",
-	inline: true,
-	inplace: true,
-	group: "mail_inline",
-	html: '<span class="[color] color"></span>'
+	...exports.color,
+	group: "mail_color"
 };
+
+exports.mail_notranslate = {
+	...exports.notranslate,
+	group: "mail_inline"
+};
+
