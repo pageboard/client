ba82604ae4bc1d6cf028edd2f5e97b6e913a7e2f
Jérémy Lal <kapouer@melix.org>
2024-01-24 00:56:31 +0100

    email: fix inlines

--- packages/email/elements/inlines.js
+++ packages/email/elements/inlines.js
@@ -6,6 +6,7 @@ exports.mail_hard_break = {
 
 exports.mail_strong = {
 	...exports.strong,
+	excludes: 'mail_light',
 	group: "mail_inline"
 };
 
@@ -16,11 +17,19 @@ exports.mail_em = {
 
 exports.mail_light = {
 	...exports.light,
+	excludes: 'mail_strong',
 	group: "mail_inline"
 };
 
 exports.mail_sup = {
 	...exports.sup,
+	excludes: 'mail_sub',
+	group: "mail_inline"
+};
+
+exports.mail_sub = {
+	...exports.sub,
+	excludes: 'mail_sup',
 	group: "mail_inline"
 };
 
