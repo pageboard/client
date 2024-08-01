e4a2bf147df060bff93526aa645186df81c68710
Jérémy Lal <kapouer@melix.org>
2024-08-01 03:27:38 +0200

    write: semafor can't remove value if it equals default

--- packages/write/ui/semafor.js
+++ packages/write/ui/semafor.js
@@ -405,8 +405,7 @@ class Semafor {
 					break;
 			}
 			if (val != null) allNulls = false;
-			if (val === field.default);
-			else if (val != null || nullable) obj[name] = val;
+			if (val != null || nullable) obj[name] = val;
 		}
 		if (allNulls) return;
 		return obj;
