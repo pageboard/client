ccb25496b1a407e83c56c51207892cacfa04f245
Jérémy Lal <kapouer@melix.org>
2024-09-13 22:35:10 +0200

    postinstall upgrade and fixes

--- packages/html/elements/page.js
+++ packages/html/elements/page.js
@@ -1,20 +1,16 @@
-exports.page.stylesheets = [
-	...exports.page.stylesheets,
+exports.page.stylesheets.push(
 	'../ui/components/reset.css',
 	'../ui/site.css',
 	'../ui/page.css',
 	'../ui/transition.css'
-];
+);
 
-exports.page.resources = {
-	reset: '../ui/components/reset.css',
-	site: '../ui/site.css'
-};
+exports.page.resources.reset = '../ui/components/reset.css';
+exports.page.resources.site = '../ui/site.css';
 
-exports.page.scripts = [
-	...exports.page.scripts,
+exports.page.scripts.push(
 	'../ui/transition.js'
-];
+);
 
 exports.page.properties.transition = {
 	title: 'Transition',
