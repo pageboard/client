8866a6c3bef501a51910a8a9e5f0f5a3b589a841
Jérémy Lal <kapouer@melix.org>
2024-10-07 00:08:00 +0200

    consent: more fixes

--- packages/html/ui/consent.js
+++ packages/html/ui/consent.js
@@ -3,20 +3,23 @@ class HTMLElementConsent extends Page.create(HTMLFormElement) {
 		dataTransient: false
 	};
 
-	static explicit;
+	static explicits = new Set();
 
-	static ask(consent) {
+	static ask(state, consent) {
 		let tacit = true;
 		const forms = document.querySelectorAll('[block-type="consent_form"]');
-		this.explicit = forms.length > 0;
+		const consents = state.scope.storage.all();
 		for (const node of forms) {
+			window.HTMLElementForm.prototype.fill.call(node, consents);
 			node.classList.add('visible');
 			tacit = consent && !node.querySelector(`[name="${consent}"]`) || false;
 		}
+		if (!tacit) this.explicits.add(consent);
 		return tacit ? "yes" : null;
 	}
 	setup(state) {
 		if (state.scope.$write) return;
+		this.constructor.explicits = new Set();
 		const view = this.ownView;
 		view.textContent = '';
 		const tmpl = this.ownTpl.prerender();
@@ -24,8 +27,11 @@ class HTMLElementConsent extends Page.create(HTMLFormElement) {
 		state.chain('consent', this);
 	}
 	chainConsent(state) {
-		window.HTMLElementForm.prototype.fill.call(this, state.scope.storage.all());
-		if (this.options.transient) this.classList.remove('visible');
+		if (this.options.transient) {
+			this.classList.remove('visible');
+		} else {
+			window.HTMLElementForm.prototype.fill.call(this, state.scope.storage.all());
+		}
 	}
 	handleChange(e, state) {
 		if (e.type == "submit" || !this.elements.find(item => item.type == "submit")) {
@@ -36,6 +42,10 @@ class HTMLElementConsent extends Page.create(HTMLFormElement) {
 		if (e.type == "submit") e.preventDefault();
 		if (state.scope.$write) return;
 		const consents = window.HTMLElementForm.prototype.read.call(this);
+		if (Array.from(this.constructor.explicits).some(c => consents[c] == null)) {
+			// not all explicit consents have been answered
+			return;
+		}
 		for (const [key, val] of Object.entries(consents)) {
 			state.scope.storage.set(key, val);
 		}
@@ -63,7 +73,7 @@ Page.constructor.prototype.consent = function (listener, ask) {
 	}
 	const cur = this.scope.storage.get(consent);
 	if (cur == null || ask) {
-		this.scope.storage.set(consent, HTMLElementConsent.ask(consent));
+		this.scope.storage.set(consent, HTMLElementConsent.ask(this, consent));
 	}
 	this.chain('consent', listener);
 };
@@ -73,7 +83,7 @@ Page.define(`element-consent`, HTMLElementConsent, 'form');
 
 Page.paint(state => {
 	state.finish(() => {
-		if (!HTMLElementConsent.explicit) {
+		if (!HTMLElementConsent.explicits.size) {
 			state.copy().runChain('consent');
 		}
 	});
