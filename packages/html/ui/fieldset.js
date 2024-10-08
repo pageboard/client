cb3b290c93801191e28854ad35caaa2814bb6a8a
Jérémy Lal <kapouer@melix.org>
2024-10-08 12:29:20 +0200

    fieldset: don't depend on patch before setup

--- packages/html/ui/fieldset.js
+++ packages/html/ui/fieldset.js
@@ -1,19 +1,15 @@
 class HTMLCustomFieldSetElement extends HTMLFieldSetElement {
-	static defaults = {
-		dataName: null,
-		dataValue: null
-	};
 	constructor() {
 		super();
 		this.init?.();
 	}
 
 	#update() {
-		if (this.isContentEditable || !this.options.name) return;
-		const name = this.options.name.split(".").slice(1).join('.');
+		if (this.isContentEditable || !this.dataset.name) return;
+		const name = this.dataset.name.split(".").slice(1).join('.');
 		const vals = this.form ? window.HTMLCustomFormElement.prototype.read.call(this.form, true) : {};
 		const val = vals[name];
-		this.disabled = this.hidden = val != this.options.value;
+		this.disabled = this.hidden = val != this.dataset.value;
 	}
 	patch() {
 		this.#update();
