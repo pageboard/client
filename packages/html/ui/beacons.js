b5ebd84f35197fa7a991e98f1b4e582e0809e1a9
Jérémy Lal <kapouer@melix.org>
2024-10-28 17:25:48 +0100

    html beacons: track $query

--- packages/html/ui/beacons.js
+++ packages/html/ui/beacons.js
@@ -1,6 +1,20 @@
 class HTMLElementBeacons extends Page.create(HTMLMetaElement) {
+	patch(state) {
+		if (!this.content) return;
+		const scope = state.scope.copy();
+		const collector = state.collector();
+		scope.$hooks = {
+			...scope.$hooks,
+			afterAll(ctx, v) {
+				collector.filter(ctx, v);
+			}
+		};
+		for (const expr of Object.values(state.parse(this.content).query)) {
+			`[${expr}]`.fuse({}, state.scope);
+		}
+	}
 	paint(state) {
-		if (state.scope.$write || state.scope.$read) return;
+		if (state.scope.$write || state.scope.$read || !this.content) return;
 		const payload = new URLSearchParams();
 		let i = 0;
 		for (const [key, expr] of Object.entries(state.parse(this.content).query)) {
