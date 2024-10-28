5f2704c4d7e229320a3eea64c9c670ac3e0a2224
Jérémy Lal <kapouer@melix.org>
2024-10-28 17:06:01 +0100

    html: do not send beacon when admin

--- packages/html/ui/beacons.js
+++ packages/html/ui/beacons.js
@@ -1,5 +1,6 @@
 class HTMLElementBeacons extends Page.create(HTMLMetaElement) {
 	paint(state) {
+		if (state.scope.$write || state.scope.$read) return;
 		const payload = new URLSearchParams();
 		let i = 0;
 		for (const [key, expr] of Object.entries(state.parse(this.content).query)) {
