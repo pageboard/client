0d43dace17ad9c29ab1f95c8514d85d47f099eaa
Jérémy Lal <kapouer@melix.org>
2024-12-09 10:51:21 +0100

    html consent: backport fix

--- packages/html/elements/consent.js
+++ packages/html/elements/consent.js
@@ -20,7 +20,8 @@ exports.consent_form = {
 		'content.': 'content.content'
 	},
 	html: `<form is="element-consent" class="ui form" data-transient="[transient]">
-		<x[transient|alt:template:div|at:-] block-content="content"></x[transient|alt:template:div|at:-]>
+		<template block-content="content"></template>
+		<div class="view"></div>
 	</form>`,
 	scripts: ['../ui/storage.js', '../ui/consent.js'],
 	stylesheets: ['../ui/consent.css']
